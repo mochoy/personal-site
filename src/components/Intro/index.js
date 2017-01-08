57f17e026fd148363fe90b1972ef8cb2518a6328
BobLakensi <montgomery.okdaily@gmail.com>
2017-01-08 12:28:42 -0800

    react boiler code

---
+++ src/components/Intro/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class Intro extends Component {
+	render () {
+		return (
+			<div id = "intro"> 
+				<p> Hello </p>
+			</div>
+
+		)
+	}
+}
