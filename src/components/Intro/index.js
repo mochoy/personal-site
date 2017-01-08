b820e02a4466759e9df96170112f0b89158ec862
BobLakensi <montgomery.okdaily@gmail.com>
2017-01-08 14:01:21 -0800

    added button

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -4,9 +4,12 @@ import './index.css';
 export default class Intro extends Component {
 	render () {
 		return (
-			<div id = "intro"> 
+			<div id="intro"> 
 				<p> Intro </p>
-				<p> {this.props.value} </p>				
+				<p> {this.props.value} </p>		
+				
+				<button className="button" onClick={() => console.log("clicked")}>Button</button> 		
+
 			</div>
 
 		)
