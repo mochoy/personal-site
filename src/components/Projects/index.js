55c6dd5b167254576903869278b3ed81d4fbe8d1
BobLakensi <montgomery.okdaily@gmail.com>
2017-03-04 13:04:00 -0800

    style Projects section

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -5,16 +5,19 @@ export default class Intro extends Component {
 	render () {
 		var projectItems = this.props.projectData.map(this.renderProjectItems)
 		return (
-			<div id="projects">{projectItems}</div>
+			<div id="Projects">
+				<h2 id="title">Projects</h2>
+				<div id="projects">{projectItems}</div>
+			</div>
 
 		)
 	}
 
 	renderProjectItems(item){
 		return (
-			<div id="project">
-				<h2>{item.name}</h2>
-				<p>{item.description}</p>
+			<div className="project">
+				<h4 className="projectName">{item.name}</h4>
+				<p className="projectDescription">{item.description}</p>
 				<a href={item.link}>Code</a>
 			</div>
 		)
