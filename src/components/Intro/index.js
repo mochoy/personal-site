bd6e325d75103516c606ba96732baee7056b5bb6
BobLakensi <montgomery.okdaily@gmail.com>
2017-03-04 12:06:26 -0800

    commit

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -5,12 +5,12 @@ export default class Intro extends Component {
 	render () {
 		return (
 			<div id="intro"> 
+			
 				<div id="introText"> 
-					<div id="bauhaus">
-						<img id="bauhausImg" src="assets/bauhaus.jpg"/>
-					</div>
 					<h1> Monty Choy </h1>
 				</div>
+
+				
 
 			</div>
 
