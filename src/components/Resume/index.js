4b198fe773f195cb9488e0181d699824fbd9b14f
BobLakensi <montgomery.okdaily@gmail.com>
2017-03-04 19:30:56 -0800

    resume items now className, not id, so css works properly

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -16,17 +16,17 @@ export default class Resume extends Component {
 	renderItems(item){
 		var toRender = item.items.map(function (arg) {
 			return (
-				<div id="subItems_SubContainer">
-					<h4 id="subItem_Name">{arg.name}</h4>
-					<p id="subItem_Description">{arg.description}</p>
+				<div className="subItems_SubContainer">
+					<h4 className="subItem_Name">{arg.name}</h4>
+					<p className="subItem_Description">{arg.description}</p>
 				</div>
 			)
 		})
 		
 		return (
-			<div id="subContainer">
-				<h3 id="sectionDescription">{item.name}</h3>
-				<div id="subItems_Container">{toRender}</div>
+			<div className="subContainer">
+				<h3 className="sectionDescription">{item.name}</h3>
+				<div className="subItems_Container">{toRender}</div>
 			</div>
 		)
 		
