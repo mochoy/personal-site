e5e631218bb5c85034a8e31a3e0c398c007f137b
BobLakensi <montgomery.okdaily@gmail.com>
2017-04-09 11:52:45 -0700

    all items in ul and flexbox so looks nice

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import './index.css';
 
+var i = 0;
+
 export default class Projects extends Component {
 	render () {
 		var projectItems = this.props.projectData.map(this.renderProjectItems)
 		return (
 			<div id="Projects">
 				<h2 id="title">Projects</h2>
-				<div id="projects-container">{projectItems}</div>
+				<ul id="projects-container">{projectItems}</ul>
 			</div>
 
 		)
@@ -15,13 +17,13 @@ export default class Projects extends Component {
 
 	renderProjectItems(item){
 		return (
-			<span className="project">
+			<li className="project">
 				<h4 className="projectName">{item.name}</h4>
 				<p className="nameAndLinkDivider"> | </p>
 				<a className="link" href={item.link}>Code</a>
 				<p className="projectDescription">{item.description}</p>
 
-			</span>
+			</li>
 		)
 	}
 
