3af58cd033e10e90cdbf2a30050e686ad5231f12
BobLakensi <montgomery.okdaily@gmail.com>
2017-04-09 13:56:37 -0700

    add periods to end of data json object

--- src/App.js
+++ src/App.js
@@ -5,39 +5,39 @@ import Projects from './components/Projects/index';
 let projects = [
 	{
 		"name": "NERF Ammo Counter",
-		"link": "https://github.com/etnom/ming-batt/tree/master/nerf-ammo-counter",
-		"description": "A kit for modified NERF blasters for counting darts",
-		"createdWith": "Made with Arduino and Adafruit's SPI and I2C OLED display libraries"
+		"link": "https://github.com/etnom/ming-batt/tree/master/nerf-ammo-counter.",
+		"description": "A kit for modified NERF blasters for counting darts.",
+		"createdWith": "Made with Arduino and Adafruit's SPI and I2C OLED display libraries."
 	},
 	{
 		"name": "NERF Voltmeter",
-		"link": "https://github.com/etnom/ming-batt/tree/master/Voltmeter",
-		"description": "A kit for modified electronic NERF blasters which reads and calculates electric potential difference of blaster's battery",
-		"createdWith": "Made with Arduino and Adafruit's SPI and I2C OLED display libraries"
+		"link": "https://github.com/etnom/ming-batt/tree/master/Voltmeter.",
+		"description": "A kit for modified electronic NERF blasters which reads and calculates electric potential difference of blaster's battery.",
+		"createdWith": "Made with Arduino and Adafruit's SPI and I2C OLED display libraries."
 	},
 	{
 		"name": "NERF Chrono Ammo Counter",
-		"link": "https://github.com/etnom/ming-batt/tree/master/ChronoAmmoCounter",
-		"description": "A kit for modified NERF blasters for counting darts which also tracks and calcualtes speed of dart",
+		"link": "https://github.com/etnom/ming-batt/tree/master/ChronoAmmoCounter.",
+		"description": "A kit for modified NERF blasters for counting darts which also tracks and calcualtes speed of dart.",
 		"createdWith": "Made with Arduino and Adafruit's SPI and I2C OLED display libraries"
 	},
 	{
 		"name": "Isaac Against the World",
-		"link": "https://github.com/etnom/isaac-against-the-world",
-		"description": "A 2-player bird's eye view zombie shooter web game, made with Phaser",
+		"link": "https://github.com/etnom/isaac-against-the-world.",
+		"description": "A 2-player bird's eye view zombie shooter web game, made with Phaser.",
 		"createdWith": "Made with Phaser and JavaScript"
 	},
 	{
 		"name": "King of Nepal Noodle",
-		"link": "https://github.com/etnom/king-of-nepal-noodle",
-		"description": "A real-time multiplayer Android tower defense game played in portrait orientation, made with Phaser and Firebase",
+		"link": "https://github.com/etnom/king-of-nepal-noodle.",
+		"description": "A real-time multiplayer Android tower defense game played in portrait orientation, made with Phaser and Firebase.",
 		"createdWith": "Made with Firebase, Phaser and JavaScript"
 	}, 
 	{
 		"name": "Awesome Game",
-		"link": "https://github.com/MiLeung/AwesomeGame",
-		"description": "A real-time multiplayer 2-d side-scroller shooter game",
-		"createdWith": "Made with Firebase, Phaser and JavaScript"
+		"link": "https://github.com/MiLeung/AwesomeGame.",
+		"description": "A real-time multiplayer 2-d side-scroller shooter game.",
+		"createdWith": "Made with Firebase, Phaser and JavaScript."
 	}
 ]
 
