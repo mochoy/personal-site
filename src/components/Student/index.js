2f68d117fdd9232bc2c38333c52599d4181a80e5
Monte <montgomery.okdaily@gmail.com>
2017-04-12 20:50:12 +0000

    create header for student section

--- src/components/Student/index.js
+++ src/components/Student/index.js
@@ -5,7 +5,9 @@ export default class Student extends Component {
 	render () {
 		return (
 			<div id="Student"> 
-			
+				<div id="titleContainer">
+					<h2 id="title">STUDENT</h2>
+				</div>
 			</div>
 
 		)
