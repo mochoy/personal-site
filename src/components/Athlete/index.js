810b172113a1f671d67914401d607c794fb728b3
BobLakensi <montgomery.okdaily@gmail.com>
2017-04-13 18:26:56 -0700

    add title athlete to athlete section

--- src/components/Athlete/index.js
+++ src/components/Athlete/index.js
@@ -4,8 +4,10 @@ import './index.css';
 export default class Student extends Component {
 	render () {
 		return (
-			<div id="Student"> 
-			
+			<div id="Athlete"> 
+				<div id="titleContainer">
+					<h2 id="title">ATHLETE</h2>
+				</div>
 			</div>
 
 		)
