3d29c1dac69f1d765237f255f2e8e8aacbfef4cc
BobLakensi <montgomery.okdaily@gmail.com>
2017-04-15 14:13:18 -0700

    add description for athletics section

--- src/components/Athlete/index.js
+++ src/components/Athlete/index.js
@@ -8,6 +8,9 @@ export default class Student extends Component {
 				<div id="titleContainer-athlete">
 					<h2 id="title-athlete">ATHLETE</h2>
 				</div>
+				<p className="athlete-description"> Monty has always been participating in recreational sports ever since childhood, running and playing basketball and football has always been a pastime. It wasn't until high school where Monty entered competitive sports. He currently represents Abraham Lincoln High School all year round in Boys' Varsity Tennis, Varsity Wrestling, and B-Boy/Breakdancing. 
+				</p>
+				
 			</div>
 
 		)
