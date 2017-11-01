eb3635d0055b56fd15922352faa980cac2da6eb8
BobLakensi <montgomery.okdaily@gmail.com>
2017-10-31 19:05:10 -0700

    add bg img

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -2,22 +2,24 @@ import React, { Component } from 'react';
 import './index.css';
 
 export default class Intro extends Component {
+
+						// <img src={require('../assets/background.jpg')}/>
+
 	render () {
 		return (
-			<div id="intro"> 
-			
-				<div id="introTextContainer"> 
-					<h1 id="introText"> Monty Choy </h1>
-				</div>
-				<div id="descriptionContainer"> 
-					<h4 id="descriptionText"> STUDENT | PROGRAMMER | ATHLETE </h4>
-				</div>
-				<div id="ghLink">
-					<a href="https://www.github.com/etnom"><img src="assets/github.png" height="32	px"></img> </a>
-				</div>
+			<div id="Intro"> 
+				<div id="bg">
+					<div id="introTextContainer"> 
+						<h1 id="introText"> Monty Choy </h1>
+					</div>
+					<div id="descriptionContainer"> 
+						<h4 id="descriptionText"> STUDENT | PROGRAMMER | ATHLETE </h4>
+					</div>
+					<div id="ghLink">
+						<a href="https://www.github.com/etnom"><img src="assets/github.png" height="32	px"></img> </a>
+					</div>
 
-				
-
+				</div>
 			</div>
 
 		)
