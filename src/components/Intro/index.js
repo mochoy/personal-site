9571cbf8bbec128fdcddddb2d640b3e4095dc7ee
etnom <montgomery.okdaily@gmail.com>
2017-11-05 12:22:31 -0600

    refactor assets, add new bg img

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -2,13 +2,11 @@ import React, { Component } from 'react';
 import './index.css';
 
 export default class Intro extends Component {
-
 						// <img src={require('../assets/background.jpg')}/>
-
 	render () {
 		return (
 			<div id="Intro">
-
+				
 			</div>
 
 		)
