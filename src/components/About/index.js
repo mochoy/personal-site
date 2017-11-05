16f7861bf540d324bb5871ac877056b9c7bf1c6e
etnom <montgomery.okdaily@gmail.com>
2017-11-05 12:57:43 -0600

    global styling classes, about section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,8 +5,10 @@ export default class About extends Component {
 						// <img src={require('../assets/background.jpg')}/>
 	render () {
 		return (
-			<div id="About">
-				its me
+			<div id="About" className='section'>
+				<div className="about-description">
+					<h3 className="about-title title">About</h3>
+				</div>
 			</div>
 
 		)
