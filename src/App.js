36705dbb32272abf6f284182aba6247c36317dc3
etnom <montgomery.okdaily@gmail.com>
2017-11-09 23:09:37 -0600

    add smart blaster to projects section

--- src/App.js
+++ src/App.js
@@ -15,13 +15,20 @@ const projectData = [
     "data": [
       {
           "key": 0,
+          "img": "smartBlaster.jpg",
+          "title": "Smart Blaster",
+          "description": "A hardware and software platform consisting of stackable PCB modules and a library compatible with Teensy and Arduino microcontrollers enabling ammo counting and more for modified Nerf blasters.",
+          "code": "https://github.com/etnom/smart-blaster"
+      },
+      {
+          "key": 1,
           "img": "arduinoChronograph.jpg",
           "title": "Chronograph",
           "description": "Arduino-powered blastics chronograph able to track and calculate muzzle velocity of Nerf blasters, airsoft guns, paintball guns, and more. Built with an Arduino Nano, side-looking IR emitters and receivers, I2C monochrome OLED display, PVC, and a cardboard box.",
           "code": "https://github.com/etnom/arduino-chronograph"
       },
       {
-        "key": 1,   
+        "key": 2,   
         "img": "personalsite.png",
           "title": "Personal Website",
           "description": "What you're on right now! Website to display who I am and what I have done. Made with React.",
