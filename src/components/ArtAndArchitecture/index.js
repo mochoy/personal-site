839d73db92cc6b721258ff36193ec3c7ac778264
etnom <montgomery.okdaily@gmail.com>
2017-11-10 14:19:04 -0600

    add ArtAndArchitecture component

---
+++ src/components/ArtAndArchitecture/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './index.css';
+
+export default class ArtAndArchitecture extends Component {
+	render () {
+		return (
+			<div id="ArtAndArchitecture" className='section'>
+				dfas
+			</div>
+
+		)
+	}
+}
