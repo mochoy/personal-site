74006c2c3c66f84f3c2bd16d38a0f3a423763fc8
etnom <montgomery.okdaily@gmail.com>
2017-11-10 14:58:50 -0600

    method to render artAndArchitectureItems

--- src/components/ArtAndArchitecture/index.js
+++ src/components/ArtAndArchitecture/index.js
@@ -3,11 +3,23 @@ import './index.css';
 
 export default class ArtAndArchitecture extends Component {
 	render () {
+		let data = this.props.data;
+		let artAndArchitectureItems = data.map(this.renderArtAndArchitectureItems);
 		return (
 			<div id="ArtAndArchitecture" className='section'>
-				dfas
+				<h3 className="title">Architecture</h3>
+				<div id="artAndArchitectureItems-container">
+					{artAndArchitectureItems}
+				</div>
 			</div>
+		)
+	}
 
+	renderArtAndArchitectureItems (item) {
+		return (
+			<div className="artAndArchitecture-item">
+				
+			</div>
 		)
 	}
 }
