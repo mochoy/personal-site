dc49e960326334c895501e465150b481fb3a48d9
etnom <montgomery.okdaily@gmail.com>
2017-11-09 22:00:56 -0600

    add project data to app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,36 @@ import About from './components/About/index';
 import TransitionImg from './components/TransitionImg/index';
 import Portfolio from './components/Portfolio/index';
 
-
+const projectData = [
+  {
+    "background-color": "white"
+  },
+  {
+    "data": [
+  {
+      "key": 0,
+      "img": "arduinoChronograph.jpg",
+      "title": "Chronograph",
+      "description": "Arduino-powered blastics chronograph able to track and calculate muzzle velocity of Nerf blasters, airsoft, paintball guns, and more.",
+      "code": "https://github.com/etnom/arduino-chronograph"
+  },
+  {
+    "key": 1,   
+    "img": "personalsite.png",
+      "title": "Personal Website",
+      "description": "What you're on right now! Website to display who I am and what I have done. Made with React.",
+      "code": "https://github.com/etnom/personal-site"
+  },
+  {
+    "key": 3, 
+    "img": "iatw.png",
+    "title": "Isaac Against the World",
+    "description": "A two-player bird's-eye-view shooter web game with guns, explosions, and zombies. Inspired by Boxhead. Made with Phaser and Annie.",
+    "code": "https://github.com/etnom/isaac-against-the-world"
+  }
+]
+  }
+]
 
 class App extends Component {
   render() {
