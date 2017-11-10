ec5f997515bbabae0aedbf287261dd21dba1941c
etnom <montgomery.okdaily@gmail.com>
2017-11-09 21:54:23 -0600

    add and style link to code

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -6,19 +6,22 @@ let projectData = [
   		"key": 0,
   		"img": "arduinoChronograph.jpg",
   		"title": "Chronograph",
-  		"description": "Arduino-powered blastics chronograph able to track and calculate muzzle velocity of Nerf blasters, airsoft, paintball guns, and more."
+  		"description": "Arduino-powered blastics chronograph able to track and calculate muzzle velocity of Nerf blasters, airsoft, paintball guns, and more.",
+  		"code": "https://github.com/etnom/arduino-chronograph"
 	},
 	{
  	 	"key": 1,		
  		"img": "personalsite.png",
   		"title": "Personal Website",
-  		"description": "What you're on right now! Website to display who I am and what I have done. Made with React."
+  		"description": "What you're on right now! Website to display who I am and what I have done. Made with React.",
+  		"code": "https://github.com/etnom/personal-site"
 	},
 	{
 		"key": 3,	
 		"img": "iatw.png",
 		"title": "Isaac Against the World",
-		"description": "A two-player bird's-eye-view shooter web game with guns, explosions, and zombies. Inspired by Boxhead. Made with Phaser and Annie."
+		"description": "A two-player bird's-eye-view shooter web game with guns, explosions, and zombies. Inspired by Boxhead. Made with Phaser and Annie.",
+		"code": "https://github.com/etnom/isaac-against-the-world"
 	}
 ]
 
@@ -44,6 +47,7 @@ export default class Projects extends Component {
 				>
 				<h3 className="project-title">{item.title}</h3>
 				<p className="project-description">{item.description}</p>
+				<a className="project-code" href={item.code}>Code</a>
 			</div>
 		)
 	}
