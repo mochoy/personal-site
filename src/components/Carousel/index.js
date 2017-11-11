6bd52d2a76d1407edf89f0acc08569d243b75b7a
etnom <montgomery.okdaily@gmail.com>
2017-11-10 18:37:55 -0600

    add carousel component

---
+++ src/components/Carousel/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './index.css';
+
+export default class Carousel extends Component {
+	render () {
+		let data = this.props.data;
+		return (
+			<div className="Carousel">
+
+			</div>
+		)
+	}
+}
