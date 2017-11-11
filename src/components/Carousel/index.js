528f0becc6905d09f6dd5e1865b7e42e9282a6ca
etnom <montgomery.okdaily@gmail.com>
2017-11-10 19:09:42 -0600

    event function on carousel togg btns clicked

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -5,10 +5,31 @@ export default class Carousel extends Component {
 	render () {
 		let data = this.props.data;
     let imgs = data.imgs;
+    let carouselSlides = imgs.map(this.renderCarouselSlides);
 		return (
 			<div className="Carousel">
-
+        <div className="carousel-slides-container">
+          {carouselSlides}
+          <button onClick={(e) => this.changeSlide(e, -1)}>prev</button>
+          <button onClick={(e) => this.changeSlide(e, 1)}>next</button>
+        </div>
 			</div>
 		)
 	}
+
+  renderCarouselSlides (item) {
+    return (
+      <div className="carousel-slides">
+        <img
+          className="carousel-img"
+          src={require(`assets/${item}`)}
+        />
+      </div>
+    )
+  }
+
+  changeSlide (e, dir) {
+    console.log(e);
+    console.log(dir);
+  }
 }
