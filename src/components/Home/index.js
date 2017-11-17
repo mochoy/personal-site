4af57a6e2371248deccd9ee33cfd5dba5a8ff08f
etnom <montgomery.okdaily@gmail.com>
2017-11-16 23:07:24 -0600

    dispaly and style navbar

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,6 +7,31 @@ export default class Home extends Component {
 		return (
 			<div id="Intro"
 				style={{backgroundImage: 'url(' + require('assets/bg1.jpg') + ')'}}>
+				<div
+					id="navbar"
+					className="center">
+					<h3 id="navbar-title">Monty Choy</h3>
+					<a href="default.asp"
+					className="navbar-link">
+						About
+					</a>
+					<a href="default.asp"
+					className="navbar-link">
+						Projects
+					</a>
+					<a href="default.asp"
+					className="navbar-link">
+						Blasters
+					</a>
+					<a href="default.asp"
+					className="navbar-link">
+						Architecture
+					</a>
+					<a href="default.asp"
+					className="navbar-link">
+					Tutorials
+					</a>
+				</div>
 				<div className="intro-caption">
 					<h1 className="intro-title">Monty Choy</h1>
 					<h3 className="intro-description">SOFTWARE DEVELOPER/EMBEDDED ELECTRONICS ENGINEER</h3>
