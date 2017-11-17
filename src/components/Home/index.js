b86d1fa9e93cf1ec033eaad49d6733b866811c39
etnom <montgomery.okdaily@gmail.com>
2017-11-16 23:47:42 -0600

    add mobile menu button, but not anumated yet

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,7 +2,12 @@ import React, { Component } from 'react';
 import './index.css';
 
 export default class Home extends Component {
-						// <img src={require('../assets/background.jpg')}/>
+	constructor(props) {
+    super(props);
+
+		this.animateMobileMenuIcon = this.animateMobileMenuIcon.bind(this);
+	}
+
 	render () {
 		const data = this.props.data;
 		const navbarData = data[0].navbarData;
@@ -16,6 +21,11 @@ export default class Home extends Component {
 					className="center">
 					<h3 id="navbar-title">Monty Choy</h3>
 					{navbarItems}
+					<div id="mobile-menu-icon">
+						<div className="mobile-menu-icon-bar" id="mobile-menu-icon-bar1"></div>
+						<div className="mobile-menu-icon-bar" id="mobile-menu-icon-bar2"></div>
+						<div className="mobile-menu-icon-bar" id="mobile-menu-icon-bar3"></div>
+					</div>
 				</div>
 				<div className="intro-caption">
 					<h1 className="intro-title">Monty Choy</h1>
@@ -34,5 +44,9 @@ export default class Home extends Component {
 				{item.text}
 			</a>
 		)
+	}
+
+	animateMobileMenuIcon () {
+		var i = 0;
 	}
 }
