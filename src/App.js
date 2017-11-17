4f341194d6bf33b20e871dcdc0aa04bddd5af5aa
etnom <montgomery.okdaily@gmail.com>
2017-11-16 22:38:37 -0600

    change data to match imgs

--- src/App.js
+++ src/App.js
@@ -158,14 +158,14 @@ const tutorialsData = [
     "data": [
       {
           "key": 0,
-          "img": "Awesome-Nerf-Turret.png",
+          "img": "mosfetTutorial.png",
           "title": "MOSFETs for Nerf",
           "description": "An extensive technical write-up on everything there is to know about power MOSFETs for Nerf applications.",
           "link": "https://www.reddit.com/r/Nerf/comments/6ufmm8/the_complete_nerf_blaster_mosfet_wiring_tutorial/"
       },
       {
           "key": 1,
-          "img": "VyperT28.jpg",
+          "img": "nerf-ammo-counter-tutorial.jpg",
           "title": "Let's Robot powered Nerf Turret",
           "description": "Follow this tutorial to make an internet-controlled Nerf blaster turret with Let's Robot's robot controllers.",
           "code": "https://github.com/etnom/runmyrobot",
@@ -173,7 +173,7 @@ const tutorialsData = [
       },
       {
         "key": 2,
-        "img": "SmartStrayven.jpg",
+        "img": "awesome-nerf-turret-tutorial.png",
           "title": "Nerf Ammo Counter",
           "description": "Make your own simple and cheap DIY ammo counter for Nerf blasters.",
           "code": "https://github.com/etnom/nerf-ammo-counter",
