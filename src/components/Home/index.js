bd0c6630ea0dd43c2294b1720428ecc9041b317a
etnom <montgomery.okdaily@gmail.com>
2017-11-17 21:48:02 -0600

    commented code for failed attempt to toggle fade

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -6,6 +6,13 @@ export default class Home extends Component {
     super(props);
 
 		this.animateMobileMenuIcon = this.animateMobileMenuIcon.bind(this);
+		this.onClickMobileMenuIcon = this.onClickMobileMenuIcon.bind(this);
+
+		this.state = {
+			"isMobileMenuClicked": false,
+			"introStyleState": "",
+			"introCaptionStyleeState": "intro-caption"
+		}
 	}
 
 	render () {
@@ -15,19 +22,22 @@ export default class Home extends Component {
 
 		return (
 			<div id="Intro"
+				className={this.state.introStyleState}
 				style={{backgroundImage: 'url(' + require('assets/bg1.jpg') + ')'}}>
 				<div
 					id="navbar"
 					className="center">
 					<h3 id="navbar-title">Monty Choy</h3>
 					{navbarItems}
-					<div id="mobile-menu-icon">
+					<div
+						id="mobile-menu-icon"
+						onClick={(e) => this.onClickMobileMenuIcon(e)}>
 						<div className="mobile-menu-icon-bar" id="mobile-menu-icon-bar1"></div>
 						<div className="mobile-menu-icon-bar" id="mobile-menu-icon-bar2"></div>
 						<div className="mobile-menu-icon-bar" id="mobile-menu-icon-bar3"></div>
 					</div>
 				</div>
-				<div className="intro-caption">
+				<div className={this.state.introCaptionStyleeState}>
 					<h1 className="intro-title">Monty Choy</h1>
 					<h3 className="intro-description">SOFTWARE DEVELOPER/EMBEDDED ELECTRONICS ENGINEER</h3>
 					<h3 className="intro-description">SAN FRANCISCO, CA</h3>
@@ -48,5 +58,21 @@ export default class Home extends Component {
 
 	animateMobileMenuIcon () {
 		var i = 0;
+	}
+
+	onClickMobileMenuIcon (e) {
+		// if (!this.state.isMobileMenuClicked) {
+		// 	this.setState({
+		// 		"isMobileMenuClicked": true,
+		// 		"introStyleState": "fade",
+		// 		"introCaptionStyleeState": "intro-caption-faded"
+		// 	});
+		// } else if (this.state.isMobileMenuClicked) {
+		// 	this.setState({
+		// 		"isMobileMenuClicked": false,
+		// 		"introStyleState": "",
+		// 		"introCaptionStyleeState": "intro-caption"
+		// 	});
+		// }
 	}
 }
