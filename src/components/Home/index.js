3fd8ecc7bbd00664b351cfcd7c0281a03e9ace5f
etnom <montgomery.okdaily@gmail.com>
2018-09-04 23:55:01 -0700

    remove redundant state and navbar management functions

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -6,42 +6,11 @@ export default class Home extends Component {
 	constructor(props) {
     super(props);
 
-		this.renderNavbarItems = this.renderNavbarItems.bind(this);
-
-		this.scrollToElement = this.scrollToElement.bind(this);
-
-		this.onClickMobileMenuIcon = this.onClickMobileMenuIcon.bind(this);
-		this.animateMenuIcon = this.animateMenuIcon.bind(this);
-
-		this.expandNavbar = this.expandNavbar.bind(this);
-		this.collapseNavbar = this.collapseNavbar.bind(this);
-
-		this.showCaption = this.showCaption.bind(this);
-		this.hideCaption = this.hideCaption.bind(this);
-
-		this.state = {
-			"isMobileMenuClicked": false,
-			"menuIconStyleState": "mobile-menu-icon-bar",
-
-			"introStyleState": "",
-			"navbarStyleState": "navbar",
-			"navbarLinkContainerStyleState": "navbar-link-container",
-			"navbarLinkStyleState": "navbar-link",
-
-			"introCaptionStyleState": "intro-caption",
-			"introTitleStyleState": "intro-title",
-			"introDescriptionStyleState": "intro-description"
-		}
 	}
 
 	render () {
-		const data = this.props.data;
-		const navbarData = data[0].navbarData;
-		const navbarItems = navbarData.map(this.renderNavbarItems);
-
 		return (
 			<div id="Intro"
-				className={this.state.introStyleState}
 				style={{backgroundImage: 'url(' + require('assets/bg1.jpg') + ')'}}>
 				<div id="header">
 					<h1 id="header-title">Monty Choy</h1>
@@ -52,76 +21,5 @@ export default class Home extends Component {
 		)
 	}
 
-	renderNavbarItems (item) {
-		return (
-			<a onClick={(e) => this.scrollToElement(e, item.link)}
-				className={this.state.navbarLinkStyleState}>
-				{item.text}
-			</a>
-		)
-	}
-
-	scrollToElement (e, link) {
-		scrollToElement('#' + link);
-	}
-
-	onClickMobileMenuIcon (e) {
-		if (!this.state.isMobileMenuClicked) {
-			this.animateMenuIcon(true);
-			this.expandNavbar();
-			this.hideCaption();
-		} else if (this.state.isMobileMenuClicked) {
-			this.animateMenuIcon(false);
-			this.collapseNavbar();
-			this.showCaption();
-		}
-	}
-
-	animateMenuIcon (toShow) {
-		if (toShow) {
-			this.setState({
-				"isMobileMenuClicked": true,
-				"menuIconStyleState": "mobile-menu-icon-bar mobile-menu-icon-bar-animated",
-			});
-		} else {
-			this.setState({
-				"isMobileMenuClicked": false,
-				"menuIconStyleState": "mobile-menu-icon-bar",
-			});
-		}
-		
-	}
-
-	expandNavbar () {
-		this.setState({
-			"navbarStyleState": "navbar-accordianed",
-			"navbarLinkContainerStyleState": "navbar-link-container-accordian",
-			"navbarLinkStyleState": "navbar-link navbar-link-accordianed"
-		});
-	}
-
-	collapseNavbar () {
-		this.setState({
-			"navbarStyleState": "navbar",
-			"navbarLinkContainerStyleState": "navbar-link-container-accordian",
-			"navbarLinkStyleState": "navbar-link"
-		});
-	}
-
-	showCaption () {
-		this.setState({
-			"introCaptionStyleState": "intro-caption",
-			"introTitleStyleState": "intro-title",
-			"introDescriptionStyleState": "intro-description"
-		});
-	}
-
-	hideCaption () {
-		this.setState({
-			"introCaptionStyleState": "hidden",
-			"introTitleStyleState": "hidden",
-			"introDescriptionStyleState": "hidden"
-		});
-	}
 
 }
