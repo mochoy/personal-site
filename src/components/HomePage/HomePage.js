fd4c37eb515956410c785cc6d624899915a7c67f
etnom <montgomery.okdaily@gmail.com>
2018-09-08 23:07:06 -0700

    sep multifile component for HomePage

---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,8 @@
+export default {
+  name: 'HomePage',
+  data () {
+    return {
+      msg: 'HomePage'
+    }
+  }
+}
---
+++ src/components/HomePage/HomePage.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="HomePage">
+  	<p>fdsfas</p>
+  	{{msg}}
+  </div>
+</template>
+
+<script src="./HomePage.js"></script>
+<style scoped src="./HomePage.css"></style>
