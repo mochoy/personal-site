5b93ca2437ab5528ec9d83ef30480a6097edf105
etnom <montgomery.okdaily@gmail.com>
2018-09-09 00:24:04 -0700

    rename homepage.vue to index.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HomePage from '@/components/HomePage/HomePage.vue'
+import HomePage from '@/components/HomePage/index.vue'
 
 Vue.use(Router)
 
