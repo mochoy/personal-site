90a6f50b9483dea80f39b277fedfb7c49ca3af6a
etnom <montgomery.okdaily@gmail.com>
2018-10-27 21:26:20 -0700

    tutorials data in sep data file

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -169,5 +169,39 @@ export const  data = {
 	    "description1": "A small year-round vacation cabin used for fun and relaxation away from the hustle and bustle of the busy and congested metropolitan area. This cabin is a comfortable, well-designed retreat, with an approximate area of 1600 square-feet. This is a flexible and sustainable space, taking advantage of 'green' technology, energy savings, innovative and minimal materials, and attention to space and light. Intgration with the beautiful outdoors are also central to the design.",
 	    "description2": "This project includes a scale model made of butterboard and foam-core board as well as hand-drafted scale drawings of the interior sectional views, exterior elevation views, landscape view, and plan views of each floor according to architectural design standards."
 	  }
+	],
+	tutorialsData: [
+	  {
+	    "title": "Tutorials",
+	    "backgroundColor": "white",
+	    "id": "Tutorials"
+	  },
+	  {
+	    "data": [
+	      {
+	          "key": 0,
+	          "img": "mosfetTutorial.png",
+	          "title": "MOSFETs for Nerf",
+	          "description": "An extensive technical write-up on everything there is to know about power MOSFETs for Nerf applications.",
+	          "link": "https://www.reddit.com/r/Nerf/comments/6ufmm8/the_complete_nerf_blaster_mosfet_wiring_tutorial/"
+	      },
+	      {
+	          "key": 1,
+	          "img": "awesome-nerf-turret-tutorial.png",
+	          "title": "Let's Robot powered Nerf Turret",
+	          "description": "Follow this tutorial to make an internet-controlled Nerf blaster turret with Let's Robot's robot controllers.",
+	          "code": "https://github.com/etnom/runmyrobot",
+	          "link": "https://letsrobot.readme.io/docs/awesome-nerf-turret"
+	      },
+	      {
+	        "key": 2,
+	        "img": "nerf-ammo-counter-tutorial.jpg",
+	          "title": "Nerf Ammo Counter",
+	          "description": "Make your own simple and cheap DIY ammo counter for Nerf blasters.",
+	          "code": "https://github.com/etnom/nerf-ammo-counter",
+	          "link": "https://www.reddit.com/r/Nerf/comments/5f9bjt/nerf_ammo_counter_the_ultimate_simple_cheap/"
+	      }
+	    ]
+	  }
 	]
 }
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -9,41 +9,6 @@ import Portfolio from '../Portfolio/index';
 import ArtAndArchitecture from '../ArtAndArchitecture/index';
 
 
-const tutorialsData = [
-  {
-    "title": "Tutorials",
-    "backgroundColor": "white",
-    "id": "Tutorials"
-  },
-  {
-    "data": [
-      {
-          "key": 0,
-          "img": "mosfetTutorial.png",
-          "title": "MOSFETs for Nerf",
-          "description": "An extensive technical write-up on everything there is to know about power MOSFETs for Nerf applications.",
-          "link": "https://www.reddit.com/r/Nerf/comments/6ufmm8/the_complete_nerf_blaster_mosfet_wiring_tutorial/"
-      },
-      {
-          "key": 1,
-          "img": "awesome-nerf-turret-tutorial.png",
-          "title": "Let's Robot powered Nerf Turret",
-          "description": "Follow this tutorial to make an internet-controlled Nerf blaster turret with Let's Robot's robot controllers.",
-          "code": "https://github.com/etnom/runmyrobot",
-          "link": "https://letsrobot.readme.io/docs/awesome-nerf-turret"
-      },
-      {
-        "key": 2,
-        "img": "nerf-ammo-counter-tutorial.jpg",
-          "title": "Nerf Ammo Counter",
-          "description": "Make your own simple and cheap DIY ammo counter for Nerf blasters.",
-          "code": "https://github.com/etnom/nerf-ammo-counter",
-          "link": "https://www.reddit.com/r/Nerf/comments/5f9bjt/nerf_ammo_counter_the_ultimate_simple_cheap/"
-      }
-    ]
-  }
-];
-
 export default class App extends Component {
   constructor (props) {
     super(props);
