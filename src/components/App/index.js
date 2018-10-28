596dbe455810a0fbf0bd9cfbacef71a73a4adaf0
etnom <montgomery.okdaily@gmail.com>
2018-10-27 21:25:32 -0700

    architecture data in data file

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -8,57 +8,6 @@ import TransitionImg from '../TransitionImg/index';
 import Portfolio from '../Portfolio/index';
 import ArtAndArchitecture from '../ArtAndArchitecture/index';
 
-const artAndArchitectureData = [
-  {
-    "name": "Serindipity",
-    "by": "Jet, Monty, and Cherry",
-    "imgs": [
-      {
-        "key": 0,
-        "src": "Serendipity1.jpeg"
-      },
-      {
-        "key": 1,
-        "src": "Serendipity2.jpg"
-      },
-      {
-        "key": 2,
-        "src": "Serendipity3.jpg"
-      },
-      {
-        "key": 3,
-        "src": "Serendipity5.jpg"
-      },
-    ],
-    "description1": "A Music Production Hub situated between Pier 1 and Pier 2 in Fort Mason's Historic Landmark District, residing close to the current home of the Fort Mason Center for Arts and Culture (FMCAC) which consolidates various outlets for people to explore their creative and artistic abilities. Serendipity to enhance the FMCAC's mission by creating additional space for musicians to both produce and perform their craft within a unique waterfront setting. Serendipity is a place for musically aspiring Bay Area artists to practice, hone, and challenge their capabilities in a wide-ranging environment equipped to cater to both collaborative interaction and individual experimentation. Serendipity will not only be a gathering place for those to express themselves through sound but will also be a place to learn and draw from musical history. Capitalizing on the site's public waterfront access, a public performace space is a key component of Serendipity.",
-    "description2": "Third place winner of Best Group Design in the 48th annual Architectural Foundation of San Francisco High School Design Competition.",
-    "description3": "This project includes a scale model made of butterboard and foam-core board as well as hand-drafted scale drawings of the interior sectional views, exterior elevation views, landscape view, and plan views of each floor according to architectural design standards."
-  },
-  {
-    "name": "Calaveras County Cabin",
-    "by": "Monty",
-    "imgs": [
-      {
-        "key": 0,
-        "src": "CalaverasCabin1.jpg"
-      },
-      {
-        "key": 1,
-        "src": "CalaverasCabin2.jpg"
-      },
-      {
-        "key": 2,
-        "src": "CalaverasCabin3.jpg"
-      },
-      {
-        "key": 3,
-        "src": "CalaverasCabin4.jpg"
-      },
-    ],
-    "description1": "A small year-round vacation cabin used for fun and relaxation away from the hustle and bustle of the busy and congested metropolitan area. This cabin is a comfortable, well-designed retreat, with an approximate area of 1600 square-feet. This is a flexible and sustainable space, taking advantage of 'green' technology, energy savings, innovative and minimal materials, and attention to space and light. Intgration with the beautiful outdoors are also central to the design.",
-    "description2": "This project includes a scale model made of butterboard and foam-core board as well as hand-drafted scale drawings of the interior sectional views, exterior elevation views, landscape view, and plan views of each floor according to architectural design standards."
-  }
-]
 
 const tutorialsData = [
   {
