0bdb82e5298d7f7b13c73db547c7028174fceb3e
etnom <montgomery.okdaily@gmail.com>
2018-10-27 21:21:29 -0700

    project data in sep data file

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -8,47 +8,6 @@ import TransitionImg from '../TransitionImg/index';
 import Portfolio from '../Portfolio/index';
 import ArtAndArchitecture from '../ArtAndArchitecture/index';
 
-
-const projectData = [
-  {
-    "title": "Projects",
-    "backgroundColor": "white",
-    "id": "Projects"
-  },
-  {
-    "data": [
-      {
-          "key": 0,
-          "img": "smartBlaster.jpg",
-          "title": "Smart Blaster",
-          "description": "A hardware and software platform consisting of stackable PCB modules and a library compatible with Teensy and Arduino microcontrollers enabling ammo counting and more for modified Nerf blasters.",
-          "code": "https://github.com/etnom/smart-blaster"
-      },
-      {
-          "key": 1,
-          "img": "arduinoChronograph.jpg",
-          "title": "Chronograph",
-          "description": "Arduino-powered blastics chronograph able to track and calculate muzzle velocity of Nerf blasters, airsoft guns, paintball guns, and more. Built with an Arduino Nano, side-looking IR emitters and receivers, I2C monochrome OLED display, PVC, and a cardboard box.",
-          "code": "https://github.com/etnom/arduino-chronograph"
-      },
-      {
-        "key": 2,
-        "img": "personalsite.png",
-          "title": "Personal Website",
-          "description": "What you're on right now! Website to display who I am and what I have done. Made with React.",
-          "code": "https://github.com/etnom/personal-site"
-      },
-      {
-        "key": 3,
-        "img": "iatw.png",
-        "title": "Isaac Against the World",
-        "description": "A two-player bird's-eye-view shooter web game with guns, explosions, and zombies. Inspired by Boxhead. Made with Phaser and Annie.",
-        "code": "https://github.com/etnom/isaac-against-the-world"
-      }
-    ]
-  }
-];
-
 const blasterData = [
   {
     "title": "Blasters",
