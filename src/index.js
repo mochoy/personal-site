f414f3e39fc832278097fbc20eecfdadd3d58017
etnom <montgomery.okdaily@gmail.com>
2018-10-27 17:46:37 -0700

    delete src lvl css file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './Components/App/index';
 import registerServiceWorker from './registerServiceWorker';
 
