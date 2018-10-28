a2c30aadec6b5139d6d05632bc6e82d48cba5028
etnom <montgomery.okdaily@gmail.com>
2018-10-27 17:29:24 -0700

    attempt to refactor but can't load imgs

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
 
 class App extends Component {
   render() {
+    let imgSrc = "logo.svg"
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={require(`assets/logo.svg`)} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
