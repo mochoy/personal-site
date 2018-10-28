f7852edd9c965dbe72b144720b3387579b40fc8d
etnom <montgomery.okdaily@gmail.com>
2018-10-28 01:43:17 -0700

    update about description

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,8 +8,9 @@ export default class About extends Component {
 				<div className="about-description">
 					<h4 className="about-title title">Hi!</h4>
 					<p className="about-description-text">
-							I'm Monty. I like to make cool things with electronics and code and sometimes put them in my Nerf guns. I also like to breakdance. 
+							I like to make cool things with electronics and code and sometimes put them in my Nerf guns. I also like to breakdance. 
 					</p>
+					<p>I'm a first year electrical engineer at Cal Poly SLO. I learned to code after my brother brought me to my first hackathon and got involved in electronics after he gave me an Arduino kit.</p>
 				</div>
 			</div>
 
