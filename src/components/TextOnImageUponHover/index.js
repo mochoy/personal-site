3331c4b14bf0e72d325fb024a5a1e5b8ede861b0
etnom <montgomery.okdaily@gmail.com>
2018-10-29 21:21:27 -0700

    add margin around imgs

--- src/components/TextOnImageUponHover/index.js
+++ src/components/TextOnImageUponHover/index.js
@@ -7,7 +7,7 @@ export default class TextOnImageUponHover extends Component {
 		return (
 			<div className={"overlay " + this.props.className}
 				style={{backgroundImage:  'url(' + require(`assets/images/${this.props.src}`) + ')'}}>
-				
+
 				<h3 className={"overlay-title " + this.props.titleClassName}>{this.props.title}</h3>
 				<p className={"overlay-description " + this.props.txtClassName}>{this.props.txt}</p>
 			</div>
