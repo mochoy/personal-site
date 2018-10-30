8c6753c19ba3d8ec63590f4238af709df28e73a4
etnom <montgomery.okdaily@gmail.com>
2018-10-29 21:41:04 -0700

    dynamically render title

--- src/components/TextOnImageUponHover/index.js
+++ src/components/TextOnImageUponHover/index.js
@@ -4,11 +4,21 @@ import './index.css';
 
 export default class TextOnImageUponHover extends Component {
 	render () {
+		let titleProps = this.props.title;
+		if (typeof titleProps !== 'undefined') {
+			let titleClassName = this.props.titleClassName;
+			var title = function () {
+				return (
+					<h3 className={"overlay-title " + titleClassName}>{titleProps}</h3>
+				) 
+			}();
+		}
+
 		return (
 			<div className={"overlay bg-img " + this.props.className}
 				style={{backgroundImage:  'url(' + require(`assets/images/${this.props.src}`) + ')'}}>
 
-				<h3 className={"overlay-title " + this.props.titleClassName}>{this.props.title}</h3>
+				{title}
 				<p className={"overlay-description " + this.props.txtClassName}>{this.props.txt}</p>
 			</div>
 		)
