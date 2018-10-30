e067ce8fb621015423bccaa975fb4d599f7a5421
etnom <montgomery.okdaily@gmail.com>
2018-10-29 21:14:38 -0700

    render project imgs as backgrounds

--- src/components/TextOnImageUponHover/index.js
+++ src/components/TextOnImageUponHover/index.js
@@ -5,10 +5,8 @@ import './index.css';
 export default class TextOnImageUponHover extends Component {
 	render () {
 		return (
-			<div className="text-on-image-upon-hover">
-				<img className={"text-on-image-upon-hover-img " + this.props.className}
-					src={require(`assets/images/${this.props.src}`)} 
-					alt={this.props.alt}/>
+			<div className={"text-on-image-upon-hover " + this.props.className}
+				style={{backgroundImage:  'url(' + require(`assets/images/${this.props.src}`) + ')'}}>
 				<h3 className={"overlay-title " + this.props.titleClassName}>{this.props.title}</h3>
 				<p className={"overlay-text " + this.props.txtClassName}>{this.props.txt}</p>
 			</div>
