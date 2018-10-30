337ca038969bae193a0ebdec63a0614e945e9aba
etnom <montgomery.okdaily@gmail.com>
2018-10-29 21:17:41 -0700

    render overlay text and title upon hover

--- src/components/TextOnImageUponHover/index.js
+++ src/components/TextOnImageUponHover/index.js
@@ -5,10 +5,11 @@ import './index.css';
 export default class TextOnImageUponHover extends Component {
 	render () {
 		return (
-			<div className={"text-on-image-upon-hover " + this.props.className}
+			<div className={"overlay " + this.props.className}
 				style={{backgroundImage:  'url(' + require(`assets/images/${this.props.src}`) + ')'}}>
+				
 				<h3 className={"overlay-title " + this.props.titleClassName}>{this.props.title}</h3>
-				<p className={"overlay-text " + this.props.txtClassName}>{this.props.txt}</p>
+				<p className={"overlay-description " + this.props.txtClassName}>{this.props.txt}</p>
 			</div>
 		)
 	}
