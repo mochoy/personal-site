6757ac0d05ea09cebdacfa177e2d2dce469eb2ad
etnom <montgomery.okdaily@gmail.com>
2018-11-02 00:15:59 -0700

    add link to btn

--- src/components/LinkBtn/index.js
+++ src/components/LinkBtn/index.js
@@ -7,7 +7,12 @@ export default class LinkBtn extends Component {
 	render () {
 		return (
 			<div className="LinkBtn">
-				
+				<a href={this.props.link} target="_blank">
+					<button className={"linkBtn " + this.props.className}
+						type="button">
+						{this.props.text}
+					</button>
+				</a>
 			</div>
 		)
 	}
