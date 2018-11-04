3d2f2fab54e61a41ccb766452e56346d9bec82f3
etnom <montgomery.okdaily@gmail.com>
2018-11-04 11:51:25 -0800

    sep styling for tutorials boxes

--- src/components/Tutorials/index.js
+++ src/components/Tutorials/index.js
@@ -23,7 +23,7 @@ export default class Tutorials extends Component {
 	renderTutorialBoxes(item) {
 		item.img = "tutorials/" + item.img
 		return (
-			<ProjectBox project={item}/>
+			<ProjectBox project={item} className="tutorial-box"/>
 		)
 	}
 
