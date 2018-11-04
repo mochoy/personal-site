e218b3e7ab77b316e63e71d1f40a888343252c93
etnom <montgomery.okdaily@gmail.com>
2018-11-04 11:47:38 -0800

    render projects boxes for tutorial

--- src/components/Tutorials/index.js
+++ src/components/Tutorials/index.js
@@ -5,13 +5,25 @@ import ProjectBox from '../ProjectBox/index'
 
 export default class Tutorials extends Component {
 	render () {
+		console.log()
+		let data = this.props.data[1].data;
+		let tutorialBoxes = data.map(this.renderTutorialBoxes)
+
 		return (
 			<div
 				className="section"
 				id="Tutorials">
 				<h1 className="center-text title">Tutorials</h1>
+				{tutorialBoxes}
 			</div>
 
+		)
+	}
+
+	renderTutorialBoxes(item) {
+		item.img = "tutorials/" + item.img
+		return (
+			<ProjectBox project={item}/>
 		)
 	}
 
