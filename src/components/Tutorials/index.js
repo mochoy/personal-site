fbdfacc5ca3c2dbf5a4a998af93f357777ab347d
etnom <montgomery.okdaily@gmail.com>
2018-11-04 11:31:49 -0800

    add files and create tutorials component

---
+++ src/components/Tutorials/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import './index.css';
+import ProjectBox from '../ProjectBox/index'
+
+export default class Tutorials extends Component {
+	render () {
+		return (
+			<div
+				className="section"
+				id="Tutorials">
+				
+			</div>
+
+		)
+	}
+
+}
