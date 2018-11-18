75e4804247d97539ff3b7c151f83d5ca78551962
etnom <montgomery.okdaily@gmail.com>
2018-11-17 17:40:46 -0800

    remove redundatnt styling

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,7 +6,8 @@ export default class Footer extends Component {
 	render () {
 		return (
 			<div id="Footer">
-				<p id="footer-text">© Monty Choy</p>
+				<p className="center-text">All photos taken on Huawei Mate 10 Pro with Leica dual-lens camera system.</p>
+				<p className="center-text">Monty Choy</p>
 			</div>
 		)
 	}
