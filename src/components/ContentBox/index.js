946cafa924411e8ad1515ca06434d15e668c5472
etnom <montgomery.okdaily@gmail.com>
2018-11-17 18:06:03 -0800

    rename project box css to content box

--- src/components/ContentBox/index.js
+++ src/components/ContentBox/index.js
@@ -6,7 +6,7 @@ import TextOnImageUponHover from '../TextOnImageUponHover/index';
 import LinkBtn from '../LinkBtn/index';
 
 
-export default class ProjectBox extends Component {
+export default class ContentBox extends Component {
 	render () {
 		let linkBtns = this.props.project.links.map(this.renderLinkBtns);
 
@@ -16,11 +16,11 @@ export default class ProjectBox extends Component {
 					src={this.props.project.img}
 					title={this.props.project.title}/>
 				<div className={this.props.textContainerClassName}>
-					<h2 className="project-title">{this.props.project.title}</h2>
-					<p className="project-by"><i>{this.props.project.by}</i></p>
-					<p className="project-description">{this.props.project.description}</p>
+					<h2 className="content-title">{this.props.project.title}</h2>
+					<p className="content-by"><i>{this.props.project.by}</i></p>
+					<p className="content-description">{this.props.project.description}</p>
 					<p><i>{this.props.project.event}</i></p>
-					<div className="link-btn-project-container">
+					<div className="link-btn-content-container">
 						{linkBtns}
 					</div>
 				</div>
@@ -32,17 +32,17 @@ export default class ProjectBox extends Component {
 		if (linkObj.name && linkObj.link) {
 			let className = "";
 			if (linkObj.name === "Check It Out") {
-				className = "link-btn-blue-project";
+				className = "link-btn-blue-content";
 			} else if (linkObj.name === "Code") {
-				className = "link-btn-green-project";
+				className = "link-btn-green-content";
 			} else if (linkObj.name === "Schematics") {
-				className = "link-btn-orange-project";
+				className = "link-btn-orange-content";
 			}
 
 			return (
 				<LinkBtn
 					className={"round-btn " + className}
-					textClassName={"link-btn-text-project"}
+					textClassName={"link-btn-text-content"}
 					link={linkObj.link}
 					text={linkObj.name}
 				/>
