56d13c6cd3a0e79066e0d0625496c485584e3203
etnom <montgomery.okdaily@gmail.com>
2019-01-07 00:23:15 -0800

    use see more compoenent to render see more in project

--- src/components/SeeMore/index.js
+++ src/components/SeeMore/index.js
@@ -5,7 +5,7 @@ import './index.css';
 export default class SeeMore extends Component {
 	render () {
 		return (
-			<div className="see-more-container">
+			<div className="SeeMore">
 				<h2 className="center-text see-more-text">See More</h2>
 				<p className="center-text see-more-chevron-p"><i className="center-text chevron down see-more-chevron"></i></p>
 			</div>
