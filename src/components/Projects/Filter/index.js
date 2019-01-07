cee2a8568e9babcc1e6d47d013029a9c7bef0550
etnom <montgomery.okdaily@gmail.com>
2019-01-07 08:51:03 -0800

    create files for filter component

---
+++ src/components/Projects/Filter/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+import './index.css';
+
+export default class Filter extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div className="Filter">
+        
+      </div>
+    );
+  }
+}
+
