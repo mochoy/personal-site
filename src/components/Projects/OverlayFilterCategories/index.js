db1176270bae4955de41a8245690de899e591d79
etnom <montgomery.okdaily@gmail.com>
2019-01-08 07:43:00 -0800

    render proper text in overlay filter categories

--- src/components/Projects/OverlayFilterCategories/index.js
+++ src/components/Projects/OverlayFilterCategories/index.js
@@ -10,11 +10,27 @@ export default class OverlayFilterCategories extends Component {
   }
 
   render() {
+    let categoryTitles = ["Category", "Languages", "Technologies"];
     return (
-      <div className="OverlayFilterCategories">
-        OverlayFilterCategories
+      <div className="OverlayFilterCategories project-filter-categories">
+        {
+          //map entire filterCategories arr
+          this.props.filterCategories.map((category, i) => {
+            return (
+              <div className="project-filter-categories">
+                <p className="overlay-description">{categoryTitles[i]}</p>
+                {
+                  //map sub filter categories
+                  category.map((subcategoryText) => {
+                    return <li className="overlay-description">{subcategoryText}</li>
+                  })
+                }
+              </div>
+            )
+          })
+        }
       </div>
-    );
+    )
   }
 }
 
