13441e7809f9a576469953eface3fc1d3a88a1d2
etnom <montgomery.okdaily@gmail.com>
2019-01-08 07:54:29 -0800

    styling for overlay filter text
    fix spacing, increase font size, all text in their own container

--- src/components/Projects/OverlayFilterCategories/index.js
+++ src/components/Projects/OverlayFilterCategories/index.js
@@ -12,17 +12,17 @@ export default class OverlayFilterCategories extends Component {
   render() {
     let categoryTitles = ["Category", "Languages", "Technologies"];
     return (
-      <div className="OverlayFilterCategories project-filter-categories">
+      <div className="OverlayFilterCategories">
         {
           //map entire filterCategories arr
           this.props.filterCategories.map((category, i) => {
             return (
-              <div className="project-filter-categories">
-                <p className="overlay-description">{categoryTitles[i]}</p>
+              <div className="overlay-filter-container">
+                <p className="overlay-description overlay-filter-title">{categoryTitles[i]}</p>
                 {
                   //map sub filter categories
                   category.map((subcategoryText) => {
-                    return <li className="overlay-description">{subcategoryText}</li>
+                    return <li className="overlay-description overlay-filter-li">{subcategoryText}</li>
                   })
                 }
               </div>
