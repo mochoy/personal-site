393ed7458c05355504f2ef229ae84a8f27d682b0
etnom <montgomery.okdaily@gmail.com>
2019-01-09 21:14:27 -0800

    render selectable filter categories titles

--- src/components/Projects/FilterSelect/index.js
+++ src/components/Projects/FilterSelect/index.js
@@ -12,17 +12,17 @@ export default class FilterSelect extends Component {
   render() {
     return (
       <div className="FilterSelect flex-container-center">
-        {this.renderSelectableFilterCategories(this.props.filterCategories.categories)}
-        {this.renderSelectableFilterCategories(this.props.filterCategories.languages)}
-        {this.renderSelectableFilterCategories(this.props.filterCategories.technologies)}
+        {this.renderSelectableFilterCategories(this.props.filterCategories.categories, "Categories")}
+        {this.renderSelectableFilterCategories(this.props.filterCategories.languages, "Languages")}
+        {this.renderSelectableFilterCategories(this.props.filterCategories.technologies, "Technologies")}
       </div>
     );
   }
 
-  renderSelectableFilterCategories(filterCategory) {
+  renderSelectableFilterCategories(filterCategory, title) {
     return (
       <div className="selectable-filter-category-container">
-      
+        <h3>{title}</h3>
         {//render all items
           filterCategory.map((item) => {
             return (
