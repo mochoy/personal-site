c32617d0a63ca0a6931ac30eab0fa46f3b2a274e
etnom <montgomery.okdaily@gmail.com>
2019-09-16 11:25:20 -0700

    revert back to old parallax, use city through window for bg img

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import scrollToElement from 'scroll-to-element';
-import { Parallax, Background } from 'react-parallax';
 
 import './index.css';
 
@@ -11,36 +10,9 @@ export default class Home extends Component {
 	}
 
 	render () {
-		const insideStyles = {
-		  background: "white",
-		  padding: 20,
-		  position: "absolute",
-		  // top: "50%",
-		  left: "50%",
-		  transform: "translate(-50%,-0%)"
-		};
-
 		return (
-			<div id="Intro">
-				{/*
-					style={{backgroundImage: 'url(' + require('assets/images/background/hawaii-beach.jpg') + ')'}}>
-				*/}	
-
-				<Parallax
-          blur={10}
-          bgImage={require('assets/images/background/city_through_window.jpg')}
-          bgImageAlt="city_through_window"
-          strength={200}
-          style={{ height: '100%' }}
-        > 
-        	<div style={{ height: '100px' }}>     
-	          <div style={insideStyles}>
-	        		<p>Put some text content here - even an empty div with fixed dimensions to have a height for the parallax.</p>
-	        	</div>
-        	</div>
-        </Parallax>
-
-        {/*
+			<div id="Intro"
+				style={{backgroundImage: 'url(' + require('assets/images/background/city_through_window.jpg') + ')'}}>
 				<div id="header">
 					<div id="header-text">
 						<h1 id="header-title">Monty Choy</h1>
@@ -60,7 +32,6 @@ export default class Home extends Component {
 					</div>
 					
 				</div>
-			*/}
 			</div>
 		)
 	}
