21f1e2bd79528bf7fd0b67d24589452a35ea4115
etnom <montgomery.okdaily@gmail.com>
2019-09-16 11:24:01 -0700

    add parallax with city img and react-parallax

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import scrollToElement from 'scroll-to-element';
+import { Parallax, Background } from 'react-parallax';
 
 import './index.css';
 
@@ -10,11 +11,36 @@ export default class Home extends Component {
 	}
 
 	render () {
+		const insideStyles = {
+		  background: "white",
+		  padding: 20,
+		  position: "absolute",
+		  // top: "50%",
+		  left: "50%",
+		  transform: "translate(-50%,-0%)"
+		};
+
 		return (
 			<div id="Intro">
 				{/*
 					style={{backgroundImage: 'url(' + require('assets/images/background/hawaii-beach.jpg') + ')'}}>
 				*/}	
+
+				<Parallax
+          blur={10}
+          bgImage={require('assets/images/background/city_through_window.jpg')}
+          bgImageAlt="city_through_window"
+          strength={200}
+          style={{ height: '100%' }}
+        > 
+        	<div style={{ height: '100px' }}>     
+	          <div style={insideStyles}>
+	        		<p>Put some text content here - even an empty div with fixed dimensions to have a height for the parallax.</p>
+	        	</div>
+        	</div>
+        </Parallax>
+
+        {/*
 				<div id="header">
 					<div id="header-text">
 						<h1 id="header-title">Monty Choy</h1>
@@ -34,6 +60,7 @@ export default class Home extends Component {
 					</div>
 					
 				</div>
+			*/}
 			</div>
 		)
 	}
