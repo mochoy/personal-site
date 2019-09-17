4580507933f9bb084de3fa471b97ccfb3f85c731
etnom <montgomery.okdaily@gmail.com>
2019-09-17 16:13:22 -0700

    chevron has pointer cursor on hover

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -23,7 +23,7 @@ export default class Home extends Component {
 					<h2>Hardware Hacker</h2>
 				</div>
 
-				<i id="welcome-chevron" className="chevron down"></i>
+				<i id="welcome-chevron" className="chevron down mouse-on-hover"></i>
 			</section>
 		)
 	}
