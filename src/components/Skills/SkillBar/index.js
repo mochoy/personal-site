1e9c6e4813ec52cf9d8245b15cb500666cabd300
etnom <montgomery.okdaily@gmail.com>
2019-09-18 16:13:57 -0700

    map passed in skills and render box for each skill passed in arr

--- src/components/Skills/SkillBar/index.js
+++ src/components/Skills/SkillBar/index.js
@@ -4,39 +4,47 @@ import './index.css';
 
 export default class SkillBar extends Component {
 	render () {
-		const { HEIGHT, 
-			WIDTH, 
-			CONTAINER_CLASSNAME,
-			SKILL_BAR_CLASSNAME,
-			TITLE_CLASSNAME,
-			TITLE_WIDTH,
-			BAR_CLASSNAME} = this.props;
+		console.log(this.props)
+
+		const { 
+			skills,
+			height, 
+			width, 
+			containerClassName,
+			skillBarClassName,
+			titleClassName,
+			titleWidth,
+			barClassName} = this.props;
 
 		return (
-			<div className={"" + CONTAINER_CLASSNAME}>
-				<div className={"skill-bar " + SKILL_BAR_CLASSNAME}
-					style={{
-						height: HEIGHT || "50px",
-						width: WIDTH || "100%",
+			<div className={"" + containerClassName}>
+				{skills.map(skill => {
+					return (
+						<div className={"skill-bar " + skillBarClassName}
+							style={{
+								height: height || "50px",
+								width: height || "100%",
 
-					}}>	
+							}}>	
 
-					<div className={"skill-bar-title " + TITLE_CLASSNAME}
-						style={{
-							width: TITLE_WIDTH || "100px",
-						}}>	
-						Yes
-					</div>
+							<div className={"skill-bar-title " + titleClassName}
+								style={{
+									width: titleWidth || "100px",
+								}}>	
+								Yes
+							</div>
 
-					<div className={"skill-bar-bar " + BAR_CLASSNAME}
-						style={{
-							height: "100%",
-							width: "100%",
-							backgroundColor: "red"
-						}}>
-					</div>
+							<div className={"skill-bar-bar " + barClassName}
+								style={{
+									height: "100%",
+									width: "100%",
+									backgroundColor: "red"
+								}}>
+							</div>
 
-				</div>
+						</div>
+					)
+				})}
 			</div>
 
 		)
