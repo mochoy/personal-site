45247985e3d728150d2718d5a253dc2f5056b747
etnom <montgomery.okdaily@gmail.com>
2019-09-18 16:01:12 -0700

    render custom skill bars

--- src/components/Skills/SkillBar/index.js
+++ src/components/Skills/SkillBar/index.js
@@ -4,11 +4,35 @@ import './index.css';
 
 export default class SkillBar extends Component {
 	render () {
-		return (
-			<div className="skill-bar">
-				Skill Bar
-				
+		const { HEIGHT, 
+			WIDTH, 
+			CONTAINER_CLASSNAME,
+			SKILL_BAR_CLASSNAME,
+			TITLE_CLASSNAME,
+			BAR_CLASSNAME} = this.props;
 
+		return (
+			<div className={"" + CONTAINER_CLASSNAME}>
+				<div className={"skill-bar " + SKILL_BAR_CLASSNAME}
+					style={{
+						height: HEIGHT || "50px",
+						width: WIDTH || "100%",
+
+					}}>	
+
+					<div className={"skill-bar-title " + TITLE_CLASSNAME}>
+						Yes
+					</div>
+
+					<div className={"skill-bar-bar " + BAR_CLASSNAME}
+						style={{
+							height: "100%",
+							width: "80%",
+							backgroundColor: "red"
+						}}>
+					</div>
+
+				</div>
 			</div>
 
 		)
