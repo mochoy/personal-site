51267b379cd738be2d7d8e96916737a5a5131887
etnom <montgomery.okdaily@gmail.com>
2019-09-18 15:45:33 -0700

    create, import, and render skill bar component

---
+++ src/components/Skills/SkillBar/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import './index.css';
+
+export default class SkillBar extends Component {
+	render () {
+		return (
+			<div className="skill-bar">
+				Skill Bar
+				
+
+			</div>
+
+		)
+	}
+
+}
