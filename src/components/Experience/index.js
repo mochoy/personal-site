45a5ed4e606c24b8dfa138531ce132dbfff479ae
etnom <montgomery.okdaily@gmail.com>
2019-09-18 17:33:25 -0700

    create CSS classes for dark section

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -7,8 +7,10 @@ export default class Experience extends Component {
   render() {
   	console.log(this.props)
     return (
-      <section id="Experience" className="section">
-        Experience
+      <section id="Experience" className="section-dark">
+        <h1 className="section-title">Experience</h1>
+        fasdfasdf
+        dfasdf
       </section>
     );
   }
