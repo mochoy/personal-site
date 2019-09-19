741d59f7b89fef6d9fa22bc314347e2efe263f71
etnom <montgomery.okdaily@gmail.com>
2019-09-18 17:47:04 -0700

    Update skills

--- src/assets/data/SKILLS.js
+++ src/assets/data/SKILLS.js
@@ -3,12 +3,18 @@ export const DATA = {
 		{name: "System Design", level: 95},
 	  {name: "MOSFET Drives", level: 90},
  		{name: "PCB Design", level: 90},
+ 		{name: "Embedded Programming", level: 90},
   	{name: "Motor Drives", level: 85},
 	  {name: "DC-DC Power", level: 85},
 	  {name: "AVR & STM32 MCUs", level: 70}
 	],
 	SWE_SKILLS: [
-		{name: "React", level: 85},
-  	{name: "Javascript", level: 75}
+  	{name: "Javascript", level: 95},
+		{name: "React", level: 90},
+		{name: "Python", level: 85},
+		{name: "C++", level: 85},
+		{name: "HTML/CSS", level: 85},
+		{name: "Node.js", level: 70},
+		{name: "MongoDB", level: 70},
 	]
 }
