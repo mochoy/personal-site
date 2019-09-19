c6e049371d5791d9babe751609098c53dc5a379e
etnom <montgomery.okdaily@gmail.com>
2019-09-18 18:18:45 -0700

    add content and color for experience section

--- src/assets/data/EXPERIENCE.js
+++ src/assets/data/EXPERIENCE.js
@@ -1,6 +1,13 @@
 export const DATA = [
 	{
 		key: 0,
-		name: "Apple"
+		name: "Apple",
+		location: "Cupertino, CA",
+		date: "Jan. 2019 - Sept. 2019",
+		title: "Apple TV Hardware Engineering Intern",
+		content: [
+			"Built Apple TV",
+			"Built remote"
+		]
 	}
 ]
--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -5,13 +5,37 @@ import './index.css';
 
 export default class Experience extends Component {
   render() {
-  	console.log(this.props)
+  	const DATA = this.props.data;
+
     return (
       <section id="Experience" className="section-dark">
       	<div className="section-content">
-      		<h1 className="section-title">Experience</h1>
-	        fasdfasdf
-	        dfasdf
+      		
+      		<h1 className="section-dark-title">Experience</h1>
+
+      		<div id="experience-items-container">
+      			{ DATA.map(experience => {
+	      			return (
+	      				<div className="experience-item flex-container-center">
+		      				<div className="experience-item-content left right-text">
+		      					<h3>{experience.name}</h3>
+		      					<p>{experience.location}</p>
+		      					<p>{experience.date}</p>
+		      				</div>
+
+		      				<div className="experience-item-content right">
+		      					<h3>{experience.title}</h3>
+		      					{ experience.content.map(contentItem => {
+		      						return (
+		      							<li>{contentItem}</li>
+      								)
+		      					})}
+		      				</div>
+		      			</div>
+	    				)
+	      		}) }
+      		</div>
+      		
       	</div>
        
       </section>
