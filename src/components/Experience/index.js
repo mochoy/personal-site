2a9bf6ac321b3bbb9df0f364e7f17374234846a6
etnom <montgomery.okdaily@gmail.com>
2019-09-20 10:49:36 -0700

    show and hide titles and places on mobile and desktop

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -18,7 +18,9 @@ export default class Experience extends Component {
 	      			return (
 	      				<div className="experience-item flex-container-center">
 		      				<div className="experience-item-content left">
-		      					<h3 className="experience-item-title-and-name">{experience.title + " @ " + experience.name}</h3>
+		      					<h3 className="experience-item-title-and-name">
+		      						{experience.title + " @ " + experience.name}
+	      						</h3>
 		      					<h3 className="experience-item-name">{experience.name}</h3>
 		      					<p>{experience.location}</p>
 		      					<p>{experience.date}</p>
