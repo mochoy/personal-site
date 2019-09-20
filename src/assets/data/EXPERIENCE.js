ff684a085f4093dfe9f187b59bd17eaf8b5d878b
etnom <montgomery.okdaily@gmail.com>
2019-09-19 19:03:01 -0700

    add Suild to experiences

--- src/assets/data/EXPERIENCE.js
+++ src/assets/data/EXPERIENCE.js
@@ -1,6 +1,16 @@
 export const DATA = [
 	{
 		key: 0,
+		name: "Suild",
+		location: "San Francisco, CA",
+		date: "February 2018 - Present",
+		title: "CEO",
+		content: [
+			"CEO"
+		]
+	},
+	{
+		key: 1,
 		name: "Apple",
 		location: "Cupertino, CA",
 		date: "January 2019 - September 2019",
