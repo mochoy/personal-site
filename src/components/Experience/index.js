98afb591875febd306b048378e237d9fed95135a
etnom <montgomery.okdaily@gmail.com>
2019-09-20 10:47:21 -0700

    render {title} @ {place}, add left border on entire experience item on mobile

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -18,13 +18,14 @@ export default class Experience extends Component {
 	      			return (
 	      				<div className="experience-item flex-container-center">
 		      				<div className="experience-item-content left">
-		      					<h3>{experience.name}</h3>
+		      					<h3 className="experience-item-title-and-name">{experience.title + " @ " + experience.name}</h3>
+		      					<h3 className="experience-item-name">{experience.name}</h3>
 		      					<p>{experience.location}</p>
 		      					<p>{experience.date}</p>
 		      				</div>
 
 		      				<div className="experience-item-content right">
-		      					<h3>{experience.title}</h3>
+		      					<h3 className="experience-item-title">{experience.title}</h3>
 		      					{ experience.content.map(contentItem => {
 		      						return (
 		      							<li>{contentItem}</li>
