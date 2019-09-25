ec23f8f0ac93baf18d924a42f9b31bd22c6ce0b1
etnom <montgomery.okdaily@gmail.com>
2019-09-24 21:49:46 -0700

    add projects data to projects json file

---
+++ src/assets/data/PROJECTS.js
@@ -0,0 +1,642 @@
+export const  data = [
+		{
+		  "key": 0,
+		  "img": "lipocalculator.png",
+		  "title": "LiPo Calculator",
+		  "date": "December 2018",
+		  "description": "A web application to calculate voltage and output current specs based on cell count, discharge rating, and capacity ratings of a LiPo.",
+		  "links": [
+		  	{
+		  		"name": "Check It Out",
+		  		"link": "https://suild.com/tools/lipo-calculator"
+		  	}
+  		],
+  		filterCategories: [
+  			[
+			 		"Web",
+			 		"Software"
+			 	],
+  			[
+  				"JavaScript",
+  				"CSS"
+			 	],
+			 	[
+			 		"React",
+			 		"Node.js",
+			 		"Express",
+			 		"MongoDB"
+			 	]
+  		]
+		},
+		{
+		  "key": 0,
+		  "img": "egg-robot.jpg",
+		  "title": "Egg Transporter Robot",
+		  "by": "Drew, Jonathon, Monty, & Jackson",
+		  "date": "October - December 2018",
+		  "description": "Line-following and obstacle-avoiding robot that transports to radiated factory workers.",
+		  "event": "Cal Poly Intro to Electrical Engineering Lab",
+		  "links": [
+		  	{
+		  		"name": "Code",
+		  		"link": "https://github.com/mochoy/ee151-labs/blob/master/EggRobot/EggRobot.ino"
+		  	}
+  		],
+  		filterCategories: [
+  			[
+			 		"Hardware",
+			 		"Software",
+			 		"Electronics"
+			 	],
+  			[
+  				"Arduino",
+  				"C++"
+			 	],
+			 	[
+			 		"Arduino"
+			 	]
+  		]
+		},
+		{
+		  "key": 0,
+		  "img": "lipochecker.png",
+		  "title": "LiPo Checker",
+		  "date": "November 2018",
+		  "description": "A web application to verify if a given LiPo battery works with a setup of aftermarket Nerf motors. Supports undervolting, overvolting, motor options, and more!",
+		  "links": [
+		  	{
+		  		"name": "Check It Out",
+		  		"link": "https://suild.com/tools/lipo-checker"
+		  	}
+  		],
+  		filterCategories: [
+  			[
+			 		"Web",
+			 		"Software"
+			 	],
+  			[
+  				"JavaScript",
+  				"CSS"
+			 	],
+			 	[
+			 		"React",
+			 		"Node.js",
+			 		"Express",
+			 		"MongoDB"
+			 	]
+  		]
+		},
+		{
+		  "key": 0,
+		  "img": "webControllerNerfBlaster.jpg",
+		  "title": "Web-Controlled Nerf Blaster",
+		  "date": "November 2018",
+		  "description": "Web application to control power outputs to control rate-of-fire and firing power of a highly modified Nerf blaster.",
+		  "event": "Cal Poly Arduino Fest 2018",
+		  "links": [
+		  	{
+		  		"name": "Code",
+		  		"link": "https://github.com/mochoy/cal-poly-arduino-fest"
+		  	}
+  		],
+  		filterCategories: [
+  			[
+			 		"Nerf Blasters",
+			 		"Web",
+			 		"Electronics",
+			 		"Hardware",
+			 		"Software"
+			 	],
+  			[
+  				"Arduino",
+  				"C++",
+  				"JavaScript",
+  				"CSS"
+			 	],
+			 	[
+			 		"React",
+			 		"Node.js",
+			 		"Express",
+			 		"Arduino"
+			 	]
+  		]
+		},
+		{
+		  "key": 0,
+		  "img": "dormDoorman.jpg",
+		  "title": "Dorm Doorman",
+		  "by": "Josh and Monty",
+		  "date": "November 2018",
+		  "description": "Uses machine learning and computer vision to unlock a door based upon facial recogition. Also includes Nerf blaster to engage unwanted intruders.",
+		  "event": "Honorable mention at init() by SLO Hacks",
+		  "links": [
+		  	{
+		  		"name": "Code",
+		  		"link": "https://github.com/JoshGrace/SLOhacks-init"
+		  	},
+		  	{
+		  		"name": "Check It Out",
+		  		"link": "https://devpost.com/software/dorm-doorman"
+		  	}		  	
+		  ],
+  		filterCategories: [
+  			[
+			 		"Nerf Blasters",
+			 		"Electronics",
+			 		"Hardware",
+			 		"Software"
+			 	],
+  			[
+  				"Python",
+  				"Arduino",
+  				"C++"
+			 	],
+			 	[
+			 		"Arduino",
+			 		"OpenCV"
+			 	]
+  		]
+		},
+		{
+		  "key": 0,
+		  "img": "nerfSuperSoakerTurret.jpg",
+		  "title": "Nerf Super Soaker Turret",
+		  "date": "Summer 2018",
+		  "description": "Remote-controlled motorized Nerf Super Soaker Thundershock watergun turret. Controlled with a Raspberry Pi with with an onboard camera and microphone allowing for realtime audio and video streaming of water blasting.",
+		  "links": [
+		  		  	
+		  ],
+  		filterCategories: [
+  			[
+  				"Nerf Blasters",
+			 		"Electronics",
+			 		"Hardware"
+			 	],
+			 	[
+			 		"Python"
+			 	],
+			 	[
+			 		"Raspberry Pi"
+			 	]
+  		]
+		},
+		{
+		  "key": 0,
+		  "img": "smartstryfe.jpg",
+		  "title": "Smart Stryfe",
+		  "date": "Summer 2018",
+		  "description": "Modified Arduino-powered Nerf Stryfe blaster. Includes a built-in ammo counter, chronograph, voltmeter, and variable motor control. Utilizes custom 3D printed and machined components specially designed for modified Nerf applications and custom Arduino shield.",
+		  "links": [
+	  	 	{
+	  	 		"name": "Code",
+	  	 		"link": "https://github.com/mochoy/smart-nerf-stryfes/tree/master/V1"
+	  	 	}
+		  ],
+  		filterCategories: [
+  			[
+  				"Nerf Blasters",
+  				"Electronics",
+  				"PCB Design",
+  				"Hardware",
+  				"Software"
+			 	],
+			 	[
+			 		"C++",
+			 		"Arduino",
+			 		"Prolog"
+			 	],
+			 	[
+			 		"Arduino"
+			 	]
+  		]
+		},
+		{
+		  "key": 0,
+		  "img": "mosfetBoardKit.png",
+		  "title": "MOSFET Board Kit",
+		  "date": "January - February 2018",
+		  "description": "MOSFET board kits for modified Nerf blasters with protection from trasient inductive spiking, dV/dt, and ESD failure modes. Also logic-level compatible with necessary components to mitigate parasitic oscillation. Released as a product shipped and sold internationally.",
+		  "links": [
+		  	{
+		  		"name": "Check It Out",
+		  		"link": "https://suild.com/shop/0"
+		  	},
+		  	{
+		  		"name": "Schematics",
+		  		"link": "https://github.com/mochoy/suild-electrical-designs/blob/master/MOSFET%20Board%20v1.2.png"
+		  	}
+		  ],
+  		filterCategories: [
+  			[
+  				"Electronics",
+  				"PCB Design",
+  				"Hardware"
+			 	],
+			 	[
+			 		"Prolog"
+			 	],
+			 	[
+
+			 	]
+  		]
+		},
+		{
+		  "key": 0,
+		  "img": "motorchart.png",
+		  "title": "Interactive Motor Chart",
+		  "date": "May 2018",
+		  "description": "Interactive chart to display specifications, information, and purchase locations of aftermarket Nerf motors for flywheel system applications.",
+		  "links": [
+		  	{
+		  		"name": "Check It Out",
+		  		"link": "https://suild.com/motor-chart"
+		  	}
+		  ],
+  		filterCategories: [
+  			[
+  				"Web",
+  				"Software"
+			 	],
+			 	[
+			 		"JavaScript",
+			 		"CSS"
+			 	],
+			 	[
+			 		"React",
+			 		"Node.js",
+			 		"Express",
+			 		"MongoDB"
+			 	]
+  		]
+		},
+		{
+		  "key": 0,
+		  "img": "suildwebsite.png",
+		  "title": "Suild.com",
+		  "date": "February 2018 - Present",
+		  "description": "Website to display tutorials and technical documentation and sell custom electronics for modifed Nerf blasters. Launched to the web with over 500 monthly views and growing.",
+		  "links": [
+		  	{
+		  		"name": "Check It Out",
+		  		"link": "https://suild.com"
+		  	}
+		  ],
+  		filterCategories: [
+  			[
+  				"Web",
+  				"Software"
+			 	],
+			 	[
+			 		"JavaScript",
+			 		"HTML",
+			 		"CSS"
+			 	],
+			 	[
+			 		"React",
+			 		"Node.js",
+			 		"Express",
+			 		"MongoDB"
+			 	]
+  		]
+		},
+		{
+		  "key": 0,
+		  "img": "nerfTime.jpg",
+		  "title": "Nerf Time",
+		  "by": "Michael, Monty, & Elias",
+		  "date": "July 2018",
+		  "description": "Nerf turret controlled remotely using web app with live video steaming using a mobile device.",
+		  "event": "Rapid.io hackathon",
+		  "links": [
+		  	{
+		  		"name": "Code",
+		  		"link": "https://github.com/MiLeung/NerfTime"
+		  	}
+		  ],
+  		filterCategories: [
+  			[
+  				"Nerf Blasters",
+  				"Electronics",
+  				"Web",
+  				"Hardware",
+  				"Software",
+  				"Mobile"
+			 	],
+			 	[
+			 		"Python",
+			 		"JavaScript",
+			 		"Arduino",
+			 		"C++",
+			 		"HTML",
+			 		"CSS"
+			 	],
+			 	[
+			 		"Arduino",
+			 		"React",
+			 		"Node.js",
+			 		"Express"
+			 	]
+  		]
+		},
+		{
+		  "key": 2,
+		  "img": "arduinoChronograph.jpg",
+		  "title": "Cardboard Chrony",
+		  "date": "August 2017",
+		  "description": "Arduino-powered blastics chronograph able to track and calculate muzzle velocity of Nerf blasters, airsoft guns, paintball guns, and more. Built with an Arduino Nano, side-looking IR emitters and receivers, I2C monochrome OLED display, PVC, and a cardboard box.",
+		  "links": [
+		  	{
+		  		"name": "Code",
+		  		"link": "https://github.com/mochoy/arduino-chronograph"
+		  	}
+		  ],
+  		filterCategories: [
+  			[
+  				"Electronics",
+  				"Hardware",
+  				"Software"
+			 	],
+			 	[
+			 		"C++",
+			 		"Arduino"
+			 	],
+			 	[
+			 		"Arduino"
+			 	]
+  		]
+		},
+		{
+		  "key": 1,
+		  "img": "smartBlaster.jpg",
+		  "title": "Smart Blaster",
+		  "date": "July 2017 - Present",
+		  "description": "A hardware and software platform consisting of stackable PCB modules and a library compatible with Teensy and Arduino microcontrollers enabling ammo counting and more for modified Nerf blasters.",
+		  "links": [
+		  	{
+		  		"name": "Code",
+		  		"link": "https://github.com/mochoy/smart-blaster"
+		  	}
+		  ],
+  		filterCategories: [
+  			[
+  				"Nerf Blasters",
+  				"Electronics",
+  				"PCB Design",
+  				"Hardware",
+  				"Software"
+			 	],
+			 	[
+			 		"Arduino",
+			 		"C++",
+			 		"Prolog"
+			 	],
+			 	[
+		 			"Arduino"
+			 	]
+  		]
+		},
+		{
+		  "key": 0,
+		  "img": "Awesome-Nerf-Turret.png",
+		  "title": "Awesome Nerf Turret",
+		  "date": "Summer 2017",
+		  "description": "Remote-controlled Nerf Hyperfire turret powered by Let's Robot's realtime platform that allows for real-time streaming and control of the turret through letsrobot.tv",
+		  "event": "Made for Let's Robot",
+		  "links": [
+		  	{
+		  		"name": "Code",
+		  		"link": "https://github.com/mochoy/runmyrobot"
+		  	}
+		  ],
+  		filterCategories: [
+  			[
+  				"Nerf Blasters",
+  				"Electronics",
+  				"Web",
+  				"Hardware",
+  				"Software"
+			 	],
+			 	[
+			 		"Python"
+			 	],
+			 	[
+			 		"Raspberry Pi"
+			 	]
+  		]
+		},
+		{
+		  "key": 0,
+		  "img": "VyperT28.jpg",
+		  "title": "Vyper T-28",
+		  "date": "August 2014 - August 2017",
+		  "description": "Integration of three Nerf blasters with a built-in Arduino-powered ammo counter, chronograph, and voltemeter.",
+		  "links": [
+		  	{
+		  		"name": "Code",
+		  		"link": "https://github.com/mochoy/vyper_t-28"
+		  	},
+		  	{
+		  		"name": "Check It Out",
+		  		"link": "https://github.com/mochoy/vyper_t-28"
+		  	}
+		  ],
+  		filterCategories: [
+  			[
+  				"Nerf Blasters",
+  				"Electronics",
+  				"PCB Design",
+  				"Hardware",
+  				"Software"
+			 	],
+			 	[
+			 		"C++",
+			 		"Arduino",
+			 		"Prolog"
+			 	],
+			 	[
+			 		"Arduino"
+			 	]
+  		]
+		},
+		{
+		  "key": 0,
+		  "img": "SmartStrayven.jpg",
+		  "title": "Smart Strayven",
+		  "date": "August 2013 - Present",
+		  "description": "Integration of two Nerf blasters with a built-in Arduino-powered ammo counter, chronograph, and voltemeter.",
+		  "links": [
+		  	{
+		  		"name": "Code",
+		  		"link": "https://github.com/mochoy/smart-strayven"
+		  	}
+		  ],
+  		filterCategories: [
+  			[
+  				"Nerf Blasters",
+  				"Electronics",
+  				"PCB Design",
+  				"Hardware",
+  				"Software"
+			 	],
+			 	[
+			 		"C++",
+			 		"Arduino",
+			 		"Prolog"
+			 	],
+			 	[
+			 		"Arduino"
+			 	]
+  		]
+		},
+		{
+		  "key": 2,
+		  "img": "nerfAmmoCounter.jpg",
+		  "title": "Nerf Ammo Counter",
+		  "date": "November 2016",
+		  "description": "Arduino-powered ammo counter for modified Nerf blasters. Can store and save various magazine sizes and count up from 0. Ammo displayed on small monochrome OLED display.",
+		  "links": [
+		  	{
+		  		"name": "Code",
+		  		"link": "https://github.com/mochoy/nerf-ammo-counter"
+		  	},
+		  	{
+		  		"name": "Check It Out",
+		  		"link": "https://www.youtube.com/watch?v=rPU7j7KLtIw"
+		  	},
+		  	{
+		  		"name": "Schematics",
+		  		"link": "http://i.imgur.com/Qt0TCzn.jpg"
+		  	}
+		  ],
+  		filterCategories: [
+  			[
+  				"Nerf Blasters",
+  				"Electronics",
+  				"Hardware",
+  				"Software"
+			 	],
+			 	[
+			 		"C++",
+			 		"Arduino"
+			 	],
+			 	[
+			 		"Arduino"
+			 	]
+  		]
+		},
+		{
+			"key": 4,
+			"img": "kingOfNepalNoodle.png",
+			"title": "King of Nepal Noodle",
+			"date": "Summer 2016",
+			"description": "In Andoid game code named 'King of Nepal Noodle', evolve through the ages to defend against the enemies from stealing your treasure by controlling your primary tower, building and upgrading more towers, and setting traps in this tower defense gamed played in the portrait orientation.",
+		  "links": [
+		  	{
+		  		"name": "Code",
+		  		"link": "https://github.com/mochoy/king-of-nepal-noodle"
+		  	}
+		  ],
+  		filterCategories: [
+  			[
+  				"Software",
+  				"Web",
+  				"Games",
+  				"Mobile"
+			 	],
+			 	[
+			 		"JavaScript",
+			 		"HTML",
+			 		"CSS"
+			 	],
+			 	[
+			 		"Phaser"
+			 	]
+  		]
+		},
+		{
+			"key": 4,
+			"img": "iatw.png",
+			"title": "Isaac Against the World",
+			"by": "Annie & Monty",
+			"date": "Fall 2016",
+			"description": "A two-player bird's-eye-view shooter web game with guns, explosions, and zombies. Inspired by Boxhead. Made with Phaser.",
+			"event": "Mission Bit winter 2015, second place winner of 2016 Congressional App Challenge",
+		  "links": [
+		  	{
+		  		"name": "Code",
+		  		"link": "https://github.com/mochoy/isaac-against-the-world"
+		  	},
+		  	{
+		  		"name": "Check It Out",
+		  		"link": "https://www.youtube.com/watch?v=poX0YGmnplM"
+		  	}
+		  ],
+  		filterCategories: [
+  			[
+  				"Software",
+  				"Web",
+  				"Games"
+			 	],
+			 	[
+			 		"JavaScript",
+			 		"HTML",
+			 		"CSS"
+			 	],
+			 	[
+			 		"Phaser"
+			 	]
+  		]
+		},
+		{
+			"key": 4,
+			"img": "fps.png",
+			"title": "Zombie FPS",
+			"by": "Monty & Wesley",
+			"date": "November 2014",
+			"description": "First-person survival shooter with tons of guns and zombies powered by Unity. Survive through endless wavies of zombies.",
+			"event": "CodeDay 2014",
+		  "links": [
+		  	{
+		  		
+		  	}
+		  ],
+  		filterCategories: [
+  			[
+  				"Software",
+  				"Games"
+			 	],
+			 	[
+			 		"C#"
+			 	],
+			 	[
+			 		"Unity"
+			 	]
+  		]
+		},
+		{
+			"key": 4,
+			"img": "CryEngineFPS.png",
+			"title": "CRYENGINE FPS",
+			"by": "Monty & Oscar",
+			"date": "Winter 2013",
+			"description": "First-person survival shooter made in CRYENGINE.",
+			"event": "CodeDay 2013",
+		  "links": [
+		  	{
+		  		
+		  	}
+		  ],
+  		filterCategories: [
+  			[
+  				"Software",
+  				"Games"
+			 	],
+			 	[
+			 		
+			 	],
+			 	[
+			 		
+			 	]
+  		]
+		}
+	]
