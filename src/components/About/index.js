ce63f8ed4962561b6077f13613884c9e4fcd61a7
etnom <montgomery.okdaily@gmail.com>
2020-01-20 00:10:53 -0800

    Display about content side by side

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,17 +6,18 @@ import LinkBtn from '../LinkBtn/index';
 
 export default class About extends Component {
 	render () {
+		const contentWidth = "30em";
+
 		return (
 			<section id="About" className='section'>
 				<div className="section-content">
 					<h1 className="section-title">About Me</h1>
 
-					<div id="about-content-container">
-						<div style={{
-							width: "80%",
-							margin: "0 auto",
-							marginBottom: "2em"
-						}}>
+					<div id="about-content-container"
+						className="flex-container-center">
+
+						{/* Text Intro */}
+						<div style={{width: contentWidth}}>
 							<p>
 									I'm Monty. I'm an <strong>electrical engineer</strong> with 3+ years of practical experience and a <strong>software engineer</strong> with 7+ years of practical experience. I do <strong>hardware system integration</strong>, PCB design, <strong>power electronics</strong>, embedded programming, and <strong>full-stack web development</strong> all for my <strong>Nerf blasters</strong>. I'm currently studying electrical engineering at Cal Poly SLO. 
 							</p>
@@ -25,7 +26,11 @@ export default class About extends Component {
 							</p>
 						</div>
 
-						<div id="logos-container" className="flex-container-center">
+						{/* Links */}
+						<div id="logos-container" 
+							className="flex-container-center"
+							style={{width: contentWidth}}
+						>
 							{this.props.data.map(this.renderLogos)}
 						</div>
 
