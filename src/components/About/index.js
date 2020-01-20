74975822c1b9976bc3eca90d2e0a2576d9b3d79b
etnom <montgomery.okdaily@gmail.com>
2020-01-20 00:27:32 -0800

    Add software section in intro

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -20,12 +20,22 @@ export default class About extends Component {
 								I'm Monty. I'm currently studying <strong>electrical engineering</strong> at <strong>Cal Poly SLO</strong>
 							</p>
 
+							{/* Summary */}
 							<p>
-									I'm an <strong>electrical engineer</strong> with 4+ years of practical experience and a <strong>software engineer</strong> with 8+ years of practical experience. I do <strong>hardware system integration</strong>, PCB design, <strong>power electronics</strong>, embedded programming, and <strong>full-stack web development</strong> all for my <strong>Nerf blasters</strong>.
+									I'm an <strong>electrical engineer</strong> with 4+ years of practical experience and a <strong>software engineer</strong> with 8+ years of practical experience.
 							</p>
 
-							<p>On the <strong>hardware</strong> side, I've worked at <strong>Apple</strong> and I run <strong>Suild</strong>. I do DC-DC power electronics, high-speed digital design, MCUs, PCB layout and design, and motor and solenoid drives.</p>
+							{/* Hardware */}
+							<p>
+								On the <strong>hardware</strong> side, I've worked at <strong>Apple</strong> and I run <strong>Suild</strong>. I do DC-DC power electronics, high-speed digital design, MCUs, PCB layout and design,  motor and solenoid drives, and electronics manufacturing and testing.
+							</p>
 
+							{/* Software */}
+							<p>
+								On the <strong>software</strong> side, I primarily focus on <strong>full-stack web development</strong> with the <strong>MERN stack</strong> and bare-metal <strong>firmware</strong>, often times on my own custom hardware. In the past, I've dabbled with Android development and game development.
+							</p>
+
+							{/* Hobbies */}
 							<p>
 									In my free time, I like to breakdance, wrestle, play Yu-Gi-Oh, modify Nerf blasters, and play tennis. 
 							</p>
