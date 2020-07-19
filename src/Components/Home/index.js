ae88c7c89cdd1538f2a20644f2eed985acb29038
Monty <montgomery.okdaily@gmail.com>
2020-07-18 19:00:45 -0700

    Revert bg img to div, not img, div needed for parallax

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
-import bboyIceyImg from '../../assets/images/background/bboy-icey.jpg';
-
 import './index.css';
 
 
 const Home = props => {
   return (
     <section id="Home">
-      <img id="bg-img" src={bboyIceyImg} />
+      <div id="bg-img"
+        style={{
+          backgroundImage: 'url(' + require('assets/images/background/bboy-icey.jpg') + ')'
+        }}
+      >
+      </div>
 
-      <div id="text">
+      <div id="Home">
         Monty
       </div>
     </section>
