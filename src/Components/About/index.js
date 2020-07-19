ec4e3c3dde0ed349fd7b44769af968873a8367b0
Monty <montgomery.okdaily@gmail.com>
2020-07-19 16:16:43 -0700

    Add data for logos

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -2,6 +2,43 @@ import React from 'react';
 
 import './index.css';
 
+const logosData = [
+  {
+    name: "github",
+    imgSrc: "github.svg",
+    link: "https://github.com/mochoy"
+  }, 
+  {
+    name: "linkedin",
+    imgSrc: "linkedin.svg",
+    link: "https://www.linkedin.com/in/montychoy/"
+  },
+  {
+    name: "email",
+    imgSrc: "email.svg",
+    link: "mailto:montychoy00+personal_website@gmail.com"
+  },
+  {
+    name: "facebook",
+    imgSrc: "facebook.svg",
+    link: "https://www.facebook.com/monty.choy"
+  },
+  {
+    name: "instagram",
+    imgSrc: "instagram.svg",
+    link: "https://www.instagram.com/darn_bok_choi_is_taken/"
+  },
+  {
+    name: "paypal",
+    imgSrc: "paypal.svg",
+    link: "https://paypal.me/suild"
+  },
+  {
+    name: "suild",
+    imgSrc: "suild.png",
+    link: "https://suild.com"
+  }
+]
 
 const About = props => {
   return (
