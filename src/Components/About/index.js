e3a3924c551e8ce492c1f6e91691c30b4ab597d0
Monty <montgomery.okdaily@gmail.com>
2020-07-19 15:35:29 -0700

    Add about text for in free time

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -6,7 +6,13 @@ import './index.css';
 const About = props => {
   return (
     <section id="About">
-      About
+      <h1 className="section-title">About Me</h1>
+
+      <div>
+        <p>
+          In my free time, in addition to coding and electronics, I like to breakdance, modify Nerf guns, and play board games and Yu-Gi-Oh.
+        </p>
+      </div>
 
     </section>
   )
