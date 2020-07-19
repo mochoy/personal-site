d95359a8a2378ca81f7e63bfd1a44b8ec2d7b3bf
Monty <montgomery.okdaily@gmail.com>
2020-07-19 15:18:20 -0700

    Add comments for parts of home section

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -8,6 +8,7 @@ const Home = props => {
     <section id="Home" 
       className="flex-container-vertically-center flex-container-horizontally-center"
     >
+      {/* Background image */}
       <div id="bg-img"
         style={{
           backgroundImage: 'url(' + require('assets/images/background/bboy-icey-blurred.jpg') + ')'
@@ -16,15 +17,19 @@ const Home = props => {
       </div>
       
       <div className="text-container text-center">
+        {/* Big intro text */}
         <h1>Monty Choy</h1>
         <h2>Hardware Hacker</h2>
 
+        {/* Links to different sections */}
         <div className="links-container flex-container-horizontally-center">
           <p className="mouse-on-hover">About</p>
           <p className="mouse-on-hover">Projects</p>
           <p className="mouse-on-hover">Articles</p>
         </div>
       </div>
+
+      {/* See more cursor to go to About section */}
       
     </section>
   )
