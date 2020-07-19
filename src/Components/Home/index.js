a15b663f2a0df8e4aaa861e2944f22a6a9d4eb1f
Monty <montgomery.okdaily@gmail.com>
2020-07-18 18:58:41 -0700

    Make bg just an img, not div with bg img

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
+import bboyIceyImg from '../../assets/images/background/bboy-icey.jpg';
+
 import './index.css';
 
 
 const Home = props => {
   return (
     <section id="Home">
-      <div id="bg-img"
-        style={{
-          backgroundImage: 'url(' + require('assets/images/background/bboy-icey.jpg') + ')'
-        }}
-      >
+      <img id="bg-img" src={bboyIceyImg} />
+
+      <div id="text">
+        Monty
       </div>
     </section>
   )
