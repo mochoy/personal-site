40ae3928ee3252995540aafadba018cc7431647e
Monty <montgomery.okdaily@gmail.com>
2020-07-19 18:15:27 -0700

    Move buzzwords data to its own file in assets/data

---
+++ src/assets/data/buzzwords.js
@@ -0,0 +1,64 @@
+const buzzwords = {
+  hwe: [
+    {
+      name: "DC-DC Power Electronics",
+      rating: 4.75
+    },
+    {
+      name: "Embedded Systems",
+      rating: 4.75
+    },
+    {
+      name: "Inductive Load Drives",
+      rating: 4.5
+    },
+    {
+      name: "PCB Layout & Design",
+      rating: 4.5
+    },
+    {
+      name: "Microcontrollers",
+      rating: 4.4
+    },
+    {
+      name: "High-Speed Digital Design",
+      rating: 4.3
+    },
+    {
+      name: "Computer Architecture",
+      rating: 4.2
+    }
+  ],
+  swe: [
+    {
+      name: "JavaScript",
+      rating: 4.75
+    },
+    {
+      name: "Embedded Programming",
+      rating: 4.75
+    },
+    {
+      name: "React",
+      rating: 4.75
+    },
+    {
+      name: "Node.js",
+      rating: 4.6
+    },
+    {
+      name: "Python",
+      rating: 4.5
+    },
+    {
+      name: "C++",
+      rating: 4.4
+    },
+    {
+      name: "C",
+      rating: 4.3
+    }
+  ]
+};
+
+export default buzzwords;
