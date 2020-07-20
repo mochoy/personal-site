ee3b3be5e9dbcf955ed39c6da9a4aa737b4b27f7
Monty <montgomery.okdaily@gmail.com>
2020-07-19 22:37:17 -0700

    Add styling for dark section

--- src/Components/Experience/index.js
+++ src/Components/Experience/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
+import './index.css';
+
 
 const Experience = props => {
 
-
   return (
-    <section id="Experience" className="section">
-      Experience
+    <section id="Experience" className="section-dark">
+      <div className="section-content">
+        <h1 className="section-title">Experience</h1>
+      </div>
     </section>
   )
 };
