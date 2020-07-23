13a9f109055735653eb14d4de5ae9a3f1640597e
Monty <montgomery.okdaily@gmail.com>
2020-07-22 21:09:03 -0700

    Add and style exp content for desktop

--- src/Components/Experience/index.js
+++ src/Components/Experience/index.js
@@ -12,6 +12,38 @@ const Experience = props => {
     <section id="Experience" className="section-grey">
       <div className="section-content">
         <h1 className="section-title">Experience</h1>
+
+        {
+          experienceData.map((expData, key) => {
+            const { company, title, team, location, date, content} = expData;
+
+            return (
+              <div key={key}
+                className="experience-item flex-container-horizontally-center"
+              >
+                <div className="left">
+                  <h3>{company}</h3>
+                  <p>{location}</p>
+                  <p>{date}</p>
+                </div>
+
+                <div className="right">
+                  <h4>{title}</h4>
+                  <p>{team}</p>
+                  {
+                    content.map((contentPoint, contentKey) => {
+                      return (
+                        <li key={contentKey}>{contentPoint}</li>
+                      )
+                    })
+                  }
+                </div>
+
+              </div>
+            )
+          })
+        }
+
       </div>
     </section>
   )
