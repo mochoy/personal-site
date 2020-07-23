9a1c595c1643c0834ea83467edeaaa32d767d93a
Monty <montgomery.okdaily@gmail.com>
2020-07-22 19:14:42 -0700

    Import hwe resume and link to it in resume img

--- src/Components/About/Resumes.js
+++ src/Components/About/Resumes.js
@@ -1,23 +1,29 @@
 import React from 'react';
 
+import hweResumePdf from '../../assets/documents/Resume Monty Choy - Hardware Engineering.pdf';
+
 const Resumes = props => {
 
   return (
     <div id="Resumes">
-      <h2 className="text-center">
-            Resumes
-            <div className="flex-container-center">
-              <div>
-                <img 
-                  className="grow-on-hover resume-icon" 
-                  src={require(`assets/images/icons/pdf.svg`)} 
-                  alt={"resume-icon"}
-                />
-                <p className="center-text"></p>
-              </div>
-              
-            </div>
-          </h2>
+      <h2 className="text-center">Resumes</h2>
+      <div className="flex-container-horizontally-center">
+
+        <a className="mouse-on-hover resume-link-container" 
+          href = {hweResumePdf} 
+          target = "_blank"
+        >
+          <img 
+            className="resume-icon grow-on-hover horizontally-center" 
+            src={require(`assets/images/icons/pdf.svg`)} 
+            alt={"resume-icon"}
+          />
+          <p className="text-center">Hardware Engineering</p>
+        </a>
+        
+      </div>
+
+
     </div>
   )
 };
