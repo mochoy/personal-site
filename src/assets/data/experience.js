a701d605ef04c0d7b3105746021ae07016b7cb5f
Monty <montgomery.okdaily@gmail.com>
2020-07-22 19:49:11 -0700

    Add dummy exp data

--- src/assets/data/experience.js
+++ src/assets/data/experience.js
@@ -1,5 +1,38 @@
 const experience = [
-
+  {
+    company: "Microsoft",
+    title: "Software Engineering Intern - Surface Duo Firmware",
+    location: "Remote",
+    date: "Jun 2020 - Present",
+    content: [
+      "I code",
+      "Code more",
+      "Code EVEN more"
+    ]
+  },
+  {
+    company: "Apple",
+    title: "Hardware Engineering Intern - Apple TV Hardware Engineering",
+    location: "Cupertino, CA",
+    date: "Jan 2019 - Sept 2019 (9 months)",
+    content: [
+      "I hw",
+      "hw more",
+      "hw EVEN more"
+    ]
+  },
+  {
+    company: "Suild",
+    title: "CEO",
+    location: "San Francisco, CA",
+    date: "Feb 2018 - Present",
+    content: [
+      "I hw",
+      "I code",
+      "hw more",
+      "hw EVEN more"
+    ]
+  }
 ];
 
 export default experience;
