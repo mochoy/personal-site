88ba5358296adb279d11534f78c5067e7c94c03b
Monty <montgomery.okdaily@gmail.com>
2020-07-22 21:19:39 -0700

    Add mobile version for exp section

--- src/Components/Experience/index.js
+++ src/Components/Experience/index.js
@@ -17,28 +17,35 @@ const Experience = props => {
           experienceData.map((expData, key) => {
             const { company, title, team, location, date, content} = expData;
 
+            const contentPoints = content.map((contentPoint, contentKey) => {
+              return (
+                <li key={contentKey}>{contentPoint}</li>
+              )
+            });
+
             return (
-              <div key={key}
-                className="experience-item-desktop flex-container-horizontally-center"
-              >
-                <div className="left">
+              <div key={key}>
+                { /* Exp item for desktop */ }
+                <div className="experience-item-desktop flex-container-horizontally-center">
+                  <div className="left">
+                    <h3>{company}</h3>
+                    <p>{location}</p>
+                    <p>{date}</p>
+                  </div>
+
+                  <div className="right">
+                    <h4>{title}</h4>
+                    <p>{team}</p>
+                    {contentPoints}
+                  </div>
+                </div>
+
+                { /* Exp item for mobile */ }
+                <div className="experience-item-mobile">
                   <h3>{company}</h3>
-                  <p>{location}</p>
-                  <p>{date}</p>
-                </div>
-
-                <div className="right">
                   <h4>{title}</h4>
-                  <p>{team}</p>
-                  {
-                    content.map((contentPoint, contentKey) => {
-                      return (
-                        <li key={contentKey}>{contentPoint}</li>
-                      )
-                    })
-                  }
+                  {contentPoints}
                 </div>
-
               </div>
             )
           })
