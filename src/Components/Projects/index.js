a471372d0766db1ffba317507d40e27f07f9b2d7
Monty <montgomery.okdaily@gmail.com>
2020-07-23 21:55:37 -0700

    Display featured projects and image

--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -6,13 +6,35 @@ import './index.css';
 const Projects = props => {
   const { projectsData } = props;
 
-  console.log(projectsData)
+  console.log()
 
   return (
     <section id="Projects" className="section">
       <div className="section-content">
         <h1 className="section-title">Projects</h1>
       </div>
+
+      {/* Featured projects */}
+      {
+        projectsData
+          .filter(project => project.isFeatured)
+          .map((project, key) => {
+            const { title, by, event, img, date, description} = project;
+
+            console.log(img)
+
+            return (
+              <div key={key}
+                className="featured-project-container horizontally-center flex-container-horizontally-center" 
+              >
+                <img alt={title + "-img"}
+                  src={require("../../assets/images/projects/" + img)} 
+                />
+                {title}
+              </div>
+            );
+          })
+      }
     </section>
   )
 };
