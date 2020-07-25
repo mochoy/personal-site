bc96f3ad97222165090fe154c80b8d199196fd47
Monty <montgomery.okdaily@gmail.com>
2020-07-25 16:23:43 -0700

    Decompose Content to its own component

--- src/Components/Projects/FeaturedProjectBox.js
+++ src/Components/Projects/FeaturedProjectBox.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Icons from './Icons';
 import Links from './Links';
 
+
 const FeaturedProjectBox = props => {
   const { project, index } = props;
 
@@ -22,8 +23,6 @@ const FeaturedProjectBox = props => {
   // Refers to text on left or right of text
   const direction = index % 2 === 1 ? "left" : "right";
 
-  
-
   return (
     <div key={index}
       className={`FeaturedProject ${direction} horizontally-center`} 
@@ -36,20 +35,18 @@ const FeaturedProjectBox = props => {
               backgroundImage: 'url(' + require(`../../assets/images/projects/${img}`)
             }}
           />
-          <div className="content">
-            <h2>{title}</h2>
-
-            <Icons direction={direction} 
-              by={by} 
-              date={date} 
-              event={event} 
-              prize={prize}
-            />
-
-            <p>{description}</p>
-
-            <Links open={open} files={files} direction={direction}/>
-          </div>
+          
+          <Content
+            direction={direction}
+            title={title}
+            by={by}
+            date={date} 
+            event={event}
+            prize={prize}
+            description={description} 
+            open={open}
+            files={files}
+          />
         </div>
       }
       {
@@ -65,6 +62,35 @@ const FeaturedProjectBox = props => {
       }
       
       
+    </div>
+  )
+};
+
+const Content = props => {
+  const { direction, 
+    title, 
+    by, 
+    date, 
+    event, 
+    prize, 
+    description, 
+    open, 
+    files } = props;
+
+  return (
+    <div className="content">
+      <h2>{title}</h2>
+
+      <Icons direction={direction} 
+        by={by} 
+        date={date} 
+        event={event} 
+        prize={prize}
+      />
+
+      <p>{description}</p>
+
+      <Links open={open} files={files} direction={direction}/>
     </div>
   )
 }
