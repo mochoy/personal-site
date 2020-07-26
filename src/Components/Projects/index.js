508aaa88e1f33238e65113ffe89c3bce38c9b45b
Monty <montgomery.okdaily@gmail.com>
2020-07-25 18:03:56 -0700

    Display number of projects in section title

--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -11,7 +11,14 @@ const Projects = props => {
   return (
     <section id="Projects" className="section">
       <div className="section-content">
-        <h1 className="section-title">Projects</h1>
+        <div className="section-title-container flex-container-horizontally-center flex-container-vertically-center">
+          <h1>Projects</h1>
+          <p style={{marginLeft: "0.5em"}}>
+            {`(${projectsData.length})`}
+          </p>
+        </div>
+
+        <h2 className="text-center">Featured Projects</h2>
 
         {/* Featured projects */}
         { projectsData
