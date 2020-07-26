8c28c4ca203ac3a1400d260d5bc9af4ec7327bfc
Monty <montgomery.okdaily@gmail.com>
2020-07-26 13:06:05 -0700

    Add event and categories for suild

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -464,13 +464,14 @@ const projects = [
     img: "suild_website.png",
     title: "Suild.com",
     date: "February 2018 - Present",
+    event: "Suild",
     isFeatured: true,
     description: "E-commerce webstore to sell custom designed PCB-based electronics, host interactive web applications, and provide technical documentation. Deployed to the web with over 20,000 annual sessions.",
     builtWith: ["React", "Node.js", "Express", "MongoDB"],
     links: {
       open: "https://suild.com",
     },
-    categories: "",
+    categories: ["Software"],
     filterCategories: [
       [
         "Web",
