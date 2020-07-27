5456fcd7238f8eb29296b3ca484a11523e55ccde
Monty <montgomery.okdaily@gmail.com>
2020-07-26 19:41:34 -0700

    Remove default cra stuff

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -20,7 +20,7 @@ function App() {
   return (
     <div className="App">
       <Home/>
-      
+
       <About
         logosData={logos}
         buzzwords={buzzwords}
@@ -35,21 +35,6 @@ function App() {
       />
 
       <Footer/>
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
