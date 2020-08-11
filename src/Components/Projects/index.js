b5032d73cdfafed0208cf7c2cf43935c9ae08957
Monty <montgomery.okdaily@gmail.com>
2020-08-10 21:28:54 -0700

    Add new filter category for Nerf

--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -10,7 +10,7 @@ import Project from './Project';
 import './index.css';
 
 
-const filterOptions = ["All", "Hardware", "Software", "Web"];
+const filterOptions = ["All", "Hardware", "Software", "Web", "Nerf"];
 
 const Projects = props => {
   const { projectsData } = props;
