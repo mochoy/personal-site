e9dcc9524afbe1e1782493f6c279cb3e98a930c3
Monty <montgomery.okdaily@gmail.com>
2020-08-16 15:53:46 -0700

    Display left and right bars on hover of img with more than 1 img

--- src/Components/Projects/BgImg.js
+++ src/Components/Projects/BgImg.js
@@ -2,15 +2,27 @@ import React from 'react';
 
 
 const BgImg = props => {
-  const { imgSrc } = props;
+  const { imgSrc, hasMultipleImgs, setImgToDisplay } = props;
+
+  if (hasMultipleImgs) {
+    console.log("hasMultipleImgs")
+  }
 
   return (
     <div className="bg-img"
       style={{
         backgroundImage: 'url(' + require(`../../assets/images/projects/${imgSrc}`)
       }}
-    />
+    >
+      { !!hasMultipleImgs &&
+        <div className="flex-container">
+          <div className="left-select mouse-on-hover"/>
+          <div className="right-select mouse-on-hover"/>
+        </div>
+      }
+      
+    </div>
   )
-};
+}; 
 
 export default BgImg;
