34f8ca78f5796c75eaccfd2c2824c0cbbe706f64
Monty <montgomery.okdaily@gmail.com>
2020-08-16 16:20:15 -0700

    Display arrow icons for left and right

--- src/Components/Projects/BgImg.js
+++ src/Components/Projects/BgImg.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { FaArrowCircleLeft, FaArrowCircleRight } from 'react-icons/fa';
+
 
 const BgImg = props => {
   const { imgSrc, hasMultipleImgs, setImgToDisplay } = props;
@@ -16,10 +18,15 @@ const BgImg = props => {
     >
       { !!hasMultipleImgs &&
         <div className="flex-container">
-          <div className="left-select mouse-on-hover">
-            
+          <div className="left-select mouse-on-hover flex-container-vertically-center">
+            <FaArrowCircleLeft className="icon"/>
           </div>
-          <div className="right-select mouse-on-hover"/>
+
+          <div/>
+
+          <div className="right-select mouse-on-hover  flex-container-vertically-center flex-container-right">
+            <FaArrowCircleRight className="icon"/>
+          </div>
         </div>
       }
       
