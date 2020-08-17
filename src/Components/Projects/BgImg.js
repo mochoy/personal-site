a351d743ed60eff3472b89baaa92aedec5950586
Monty <montgomery.okdaily@gmail.com>
2020-08-16 21:22:58 -0700

    Add arrows to cycle between images

--- src/Components/Projects/BgImg.js
+++ src/Components/Projects/BgImg.js
@@ -4,11 +4,16 @@ import { FaArrowCircleLeft, FaArrowCircleRight } from 'react-icons/fa';
 
 
 const BgImg = props => {
-  const { imgSrc, hasMultipleImgs, setImgToDisplay } = props;
+  const { imgSrc, 
+    setImgToDisplay, 
+    
+    hasMultipleImgs, 
+  
+    // This should only be used if hasMultipleImgs is true/there are multiple 
+    // images
+    imgs,
+    imgSrcIndex} = props;
 
-  if (hasMultipleImgs) {
-    console.log("hasMultipleImgs")
-  }
 
   return (
     <div className="bg-img"
@@ -18,15 +23,41 @@ const BgImg = props => {
     >
       { !!hasMultipleImgs &&
         <div className="flex-container">
-          <div className="left-select mouse-on-hover flex-container-vertically-center">
-            <FaArrowCircleLeft className="go-left-img-icon icon"/>
-          </div>
 
+          { /* Only display btn to go left if img being displayed isnt first img */
+            imgSrcIndex > 0 &&
+
+            <div className="left-select mouse-on-hover flex-container-vertically-center"
+              onClick={() => {
+                setImgToDisplay(imgs[imgSrcIndex - 1]);
+              }}
+            >
+              <FaArrowCircleLeft className="go-left-img-icon icon"/>
+            </div>
+          }
+
+          { /* Display dummy div if the left btn isn't visible to preserve spacing */ }
+          { imgSrcIndex <= 0 && <div/> }
+
+          { /* Center div to preserve spacing */ }
           <div/>
 
-          <div className="right-select mouse-on-hover  flex-container-vertically-center flex-container-right">
-            <FaArrowCircleRight className="go-right-img-icon icon"/>
-          </div>
+
+          { /* Only display btn to go right if img being displayed isnt last img */
+            imgSrcIndex < imgs.length - 1 &&
+
+            <div className="right-select mouse-on-hover  flex-container-vertically-center flex-container-right"
+              onClick={() => {
+                setImgToDisplay(imgs[imgSrcIndex + 1]);
+              }}
+            >
+              <FaArrowCircleRight className="go-right-img-icon icon"/>
+            </div>
+          }
+
+          { /* Display dummy div if the right btn isn't visible to preserve spacing */ }
+          { imgSrcIndex >= imgs.length - 1 && <div/> }
+
         </div>
       }
       
--- src/Components/Projects/Project/index.js
+++ src/Components/Projects/Project/index.js
@@ -14,6 +14,11 @@ const Project = props => {
 
   const hasMultipleImgs = Array.isArray(img); 
 
+  // Source of image to display, typically from project.img. This should be a 
+  // string. 
+  //
+  // Using useState hook because if there's more than 1 img for a project (img 
+  // is an arr), we want to be able to cycle between these images
   const [ imgToDisplay, setImgToDisplay ] = useState(
     // Default img should be img (if img is not arr) but if img
     // is arr, then the first item of arr is default img
@@ -25,8 +30,16 @@ const Project = props => {
     <div className="Project">
       <BgImg 
         imgSrc={imgToDisplay}
-        hasMultipleImgs={hasMultipleImgs}
         setImgToDisplay={setImgToDisplay}
+
+        hasMultipleImgs={hasMultipleImgs}
+
+        imgs={img /* This should only be used if hasMultipleImgs is true */ }
+        imgSrcIndex={ /* This should only be used if hasMultipleImgs is true */ 
+          hasMultipleImgs 
+            ? img.indexOf(imgToDisplay)
+            : 0
+        }
       />
 
       <Content
