9bd1509a090a7407e0ed5d914f6749fcf95f6e64
Monty <montgomery.okdaily@gmail.com>
2020-09-03 21:52:43 -0700

    Remove sample rate ga

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -24,7 +24,7 @@ const initGA = () => {
   ReactGA.initialize('UA-128598718-1', {
     debug: true,
     testMode: true,    // Uncomment/set to false if dont want data sent to ga
-    siteSpeedSampleRate: 100
+    // siteSpeedSampleRate: 100
   });
 
   ReactGA.pageview(window.location.pathname + window.location.search);
