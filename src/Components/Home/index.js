3f2ced182ef7ac8e1037494b0de5ada03d02db02
Monty <montgomery.okdaily@gmail.com>
2020-09-06 13:12:54 -0700

    Try to show nav from Home component after Home becomes invisible

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -15,66 +15,71 @@ const Home = props => {
   const [ isHomeVisible, setIsHomeVisible ] = React.useState(true);
 
   return (
-    <section id="Home" 
-      className="flex-container-vertically-center flex-container-horizontally-center"
-    >
-      {/* Background image */}
-      <div id="bg-img"
-        style={{
-          backgroundImage: 'url(' + require('assets/images/background/bboy-icey-blurred.jpg') + ')'
-        }}
-      >
-      </div>
-      
-      <div className="text-container text-center">
-        {/* Big intro text */}
-        <h1>Monty Choy</h1>
-        <h2>Hardware Hacker</h2>
-
-        {/* Links to different sections */}
-        <div className="links-container flex-container-horizontally-center">
-          {
-            ["About", "Experience", "Projects"]
-              .map((section, key) => {
-                return (
-                  <p className="mouse-on-hover" 
-                    onClick={e => {
-                      ReactGA.event({
-                        category: 'Home Links',
-                        action: 'Clicked',
-                        label: section,
-                      });
-
-                      scrollToElement(`#${section}`)
-                    }}
-                    key={key}
-                  >
-                    {section}
-                  </p>
-                )
-              })
-          }
-        </div>
-      </div>
+    <div style={{ height: "100%" }}>
+    { /*  This above div is needed to display the nav in this component, there
+          are performance issues with setting the state if I want to display nav 
+          from App 
+    */ }
 
       <VisibilitySensor 
-          partialVisibility={true} 
-          onChange={ isVisible => setIsHomeVisible(isVisible)}
+        partialVisibility={true} 
+        onChange={ isVisible => setIsHomeVisible(isVisible)}
+      >
+        <section id="Home" 
+          className="flex-container-vertically-center flex-container-horizontally-center"
         >
-        {/* See more cursor to go to About section */}
-        <i className="chevron down mouse-on-hover" 
-          onClick={e => scrollToElement("#About")}
-        />
-      </VisibilitySensor>
-      
+          {/* Background image */}
+          <div id="bg-img"
+            style={{
+              backgroundImage: 'url(' + require('assets/images/background/bboy-icey-blurred.jpg') + ')'
+            }}
+          >
+          </div>
+          
+          <div className="text-container text-center">
+            {/* Big intro text */}
+            <h1>Monty Choy</h1>
+            <h2>Hardware Hacker</h2>
 
+            {/* Links to different sections */}
+            <div className="links-container flex-container-horizontally-center">
+              {
+                ["About", "Experience", "Projects"]
+                  .map((section, key) => {
+                    return (
+                      <p className="mouse-on-hover" 
+                        onClick={e => {
+                          ReactGA.event({
+                            category: 'Home Links',
+                            action: 'Clicked',
+                            label: section,
+                          });
+
+                          scrollToElement(`#${section}`)
+                        }}
+                        key={key}
+                      >
+                        {section}
+                      </p>
+                    )
+                  })
+              }
+            </div>
+          </div>
+          
+          {/* See more cursor to go to About section */}
+          <i className="chevron down mouse-on-hover" 
+            onClick={e => scrollToElement("#About")}
+          />
+
+        </section>
+      </VisibilitySensor>
 
       { !isHomeVisible && 
         <Nav/>
       }
-      
-
-    </section>
+    </div>
+    
   )
 }
 
