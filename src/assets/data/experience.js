2ba74137f7f1891aa67ae2b4b386f8a380d4e0b9
Monty <montgomery.okdaily@gmail.com>
2020-09-06 21:52:26 -0700

    Update Apple exp to a paragraph

--- src/assets/data/experience.js
+++ src/assets/data/experience.js
@@ -4,24 +4,26 @@ const experience = [
     title: "Software Engineering Intern - Surface Duo Firmware",
     location: "Sunnyvale, CA (Remote)",
     date: "Jun 2020 - Sept 2020",
-    content: [
-      "Software engineering intern on Microsoft Surface Duo firmware team"
-    ]
+    content: ""
+    // [
+    //   "Software engineering intern on Microsoft Surface Duo firmware team"
+    // ]
   },
   {
     company: "Apple",
     title: "Hardware Engineering Intern - Apple TV Hardware Engineering",
     location: "Cupertino, CA",
     date: "Jan 2019 - Sept 2019 (9 months)",
-    content: [
-      "Designed electrical, mechanical, & software system to characterize IR performance",
-      "Developed embedded firmware abstraction layer on STM32F1 MCU for closed-loop motor control, LCD driver, sensor/peripheral control, & I2C & UART communication",
-      "Programmed Python application as flexible entry point to embedded systems over UART",
-      "Architected, prototyped, & designed HDMI dev platform PCB. Design lead for DC-DC power, USB, & debug & assisted with high-speed digital & MCU subsystems",
-      "Analyzed test coverage on factory line to ensure correct placement, value, etc. for every component through various test stations. Results increased component test coverage by over 25%",
-      "Led validation, debug, & FA efforts: HDMI (CTS), PMU, SoC, UART, NAND, PCIe & PDM mic hardware subsystem, multilayer PCB failure, & line analysis",
-      "Wrote Python scripts for automated data collection to measure long-term performance"
-    ]
+    content: "I did a 9-month hardware engineering co-op on the Apple TV and Apple TV Remote. I experienced Apple's hardware product design, testing, and manufacturing methodologies and how that all comes together to make amazing products. I most enjoyed exploring behind the scenes on Apple's custom SoCs, DC-DC buck converters, especially on Apple's custom PMUs, and high-speed digital design and communication on HDMI, specifically the PHYs."
+    // [
+    //   "Designed electrical, mechanical, & software system to characterize IR performance",
+    //   "Developed embedded firmware abstraction layer on STM32F1 MCU for closed-loop motor control, LCD driver, sensor/peripheral control, & I2C & UART communication",
+    //   "Programmed Python application as flexible entry point to embedded systems over UART",
+    //   "Architected, prototyped, & designed HDMI dev platform PCB. Design lead for DC-DC power, USB, & debug & assisted with high-speed digital & MCU subsystems",
+    //   "Analyzed test coverage on factory line to ensure correct placement, value, etc. for every component through various test stations. Results increased component test coverage by over 25%",
+    //   "Led validation, debug, & FA efforts: HDMI (CTS), PMU, SoC, UART, NAND, PCIe & PDM mic hardware subsystem, multilayer PCB failure, & line analysis",
+    //   "Wrote Python scripts for automated data collection to measure long-term performance"
+    // ]
   },
   {
     company: "Suild",
