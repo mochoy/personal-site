875bc1b9a40aa1ff3df3619110c711d060b3d7c9
Monty <montgomery.okdaily@gmail.com>
2020-09-08 19:22:05 -0700

    create component for gh stats

---
+++ src/Components/About/GithubStats.js
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react';
+
+import { ReactGACtx } from '../App';
+
+
+const GithubStats = props => {
+
+  return (
+    <div id="GithubStats">
+      GithubStats
+    </div>
+  )
+}
+
+export default GithubStats;
