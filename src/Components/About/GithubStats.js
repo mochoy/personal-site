6fddc9ff77e4b5177c03d7d9090f483f4812bbad
Monty <montgomery.okdaily@gmail.com>
2020-09-08 19:26:10 -0700

    Move green boxes and stuff to gh stats component

--- src/Components/About/GithubStats.js
+++ src/Components/About/GithubStats.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import GitHubCalendar from 'react-github-calendar';
 
 import { ReactGACtx } from '../App';
 
@@ -7,7 +8,35 @@ const GithubStats = props => {
 
   return (
     <div id="GithubStats">
-      GithubStats
+
+      {/* Green boxes */}
+      <div className="horizontally-center">
+        <h2 className="text-center">
+          GitHub Stats
+        </h2>
+
+        <div className="flex-container-horizontally-center">
+          <GitHubCalendar username="mochoy"/>
+        </div>
+
+        {/* Stats */}
+        <div className="flex-container-horizontally-center">
+          <a href="https://github.com/anuraghazra/github-readme-stats">
+            <img id="gh-stats-img"
+              align="center" 
+              alt="gh-stats-img"
+              src="https://github-readme-stats.anuraghazra1.vercel.app/api?username=mochoy&show_icons=true&count_private=true&hide=issues&include_all_commits=true" 
+            />
+          </a>  
+          <a href="https://github.com/anuraghazra/github-readme-stats">
+            <img id="top-languages-img"
+              align="center" 
+              alt="top-languages-img"
+              src="https://github-readme-stats.anuraghazra1.vercel.app/api/top-langs/?username=mochoy&layout=compact" 
+            />
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import GitHubCalendar from 'react-github-calendar';
 
 import Logos from './Logos';
 import Resumes from './Resumes';
@@ -55,34 +54,6 @@ const About = props => {
         />
 
         <GithubStats/>
-        
-        {/* Green boxes */}
-        <div className="horizontally-center">
-          <h2 className="text-center">
-            GitHub Stats
-          </h2>
-
-          <div className="flex-container-horizontally-center">
-            <GitHubCalendar username="mochoy"/>
-          </div>
-
-          <div className="flex-container-horizontally-center">
-            <a href="https://github.com/anuraghazra/github-readme-stats">
-              <img id="gh-stats-img"
-                align="center" 
-                alt="gh-stats-img"
-                src="https://github-readme-stats.anuraghazra1.vercel.app/api?username=mochoy&show_icons=true&count_private=true&hide=issues&include_all_commits=true" 
-              />
-            </a>  
-            <a href="https://github.com/anuraghazra/github-readme-stats">
-              <img id="top-languages-img"
-                align="center" 
-                alt="top-languages-img"
-                src="https://github-readme-stats.anuraghazra1.vercel.app/api/top-langs/?username=mochoy&layout=compact" 
-              />
-            </a>
-          </div>
-        </div>
 
       </div>
     </section>
