caf67db2dfc1cf46cc1daec36987400774d13cdf
Monty <montgomery.okdaily@gmail.com>
2020-11-25 20:45:51 -0800

    Remove links from Home because now in navbar

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,6 +1,5 @@
 import React, { useContext } from 'react';
 import scrollToElement from 'scroll-to-element';
-import { Link } from 'react-router-dom';
 
 import { ReactGACtx } from '../App';
 
@@ -41,43 +40,6 @@ const Home = props => {
         <h1>Monty Choy</h1>
         <h2>Hardware Hacker</h2>
 
-        {/* Links to different sections */}
-        <div className="links-container flex-container-horizontally-center">
-          {
-            ["About", "Experience", "Projects"]
-              .map((section, key) => {
-                return (
-                  <p className="mouse-on-hover" 
-                    onClick={e => {
-                      ReactGA.event({
-                        category: 'Home Links',
-                        action: 'Clicked',
-                        label: section,
-                      });
-
-                      scrollToElement(`#${section}`)
-                    }}
-                    key={key}
-                  >
-                    {section}
-                  </p>
-                )
-              })
-          }
-          <Link to={"/blog"}>
-            <p className="mouse-on-hover" 
-              onClick={e => {
-                ReactGA.event({
-                  category: 'Home Links',
-                  action: 'Clicked',
-                  label: "Blog",
-                });
-              }}
-            >
-              Blog
-            </p>
-          </Link>
-        </div>
       </div>
       
       {/* See more cursor to go to About section */}
