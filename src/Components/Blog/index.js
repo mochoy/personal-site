cd51f9e86f58ff293f45125956a205388e963a26
Monty <montgomery.okdaily@gmail.com>
2020-11-26 13:35:55 -0800

    Remove unused vars

--- src/Components/Blog/index.js
+++ src/Components/Blog/index.js
@@ -1,14 +1,14 @@
-import React, { useContext } from 'react';
+import React, { 
+  // useContext 
+} from 'react';
 
-import Nav from '../Nav';
-
-import { ReactGACtx } from '../App';
+// import { ReactGACtx } from '../App';
 
 import './index.css';
 
 
 const Blog = props => {  
-  const ReactGA = useContext(ReactGACtx);
+  // const ReactGA = useContext(ReactGACtx);
 
   return (
     <div id="Blog">
