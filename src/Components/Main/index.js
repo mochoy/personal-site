be3a54c2d012cb2abdcabae10ddf6db112d55bca
Monty <montgomery.okdaily@gmail.com>
2020-12-02 18:49:05 -0800

    Remove useState import in Main

--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect } from 'react';
 import VisibilitySensor from 'react-visibility-sensor';
 import scrollToElement from 'scroll-to-element';
 
