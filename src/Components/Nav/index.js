c801104623bdb9ae87e9b3e0dd6cc763bd6fdfb9
Monty <montgomery.okdaily@gmail.com>
2020-12-05 18:14:22 -0800

    Remove useEffect import in Nav, that's handled in opacity hook

--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import HamburgerMenu from 'react-hamburger-menu';
 import { Collapse } from '@material-ui/core';
