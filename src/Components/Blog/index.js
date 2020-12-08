5ec73f6eb8e298c490fb1be49c5bdbd9e40678cd
Monty <montgomery.okdaily@gmail.com>
2020-12-07 22:32:19 -0800

    import first post into Blog page

--- src/Components/Blog/index.js
+++ src/Components/Blog/index.js
@@ -4,16 +4,20 @@ import React, {
 
 // import { ReactGACtx } from '../App';
 
+import first_post from '../../assets/blog/first_post';
+
 import './index.css';
 
 
 const Blog = props => {  
   // const ReactGA = useContext(ReactGACtx);
 
+  console.log(first_post)
+
   return (
     <div id="Blog">      
       <h1 className="text-center">Posts</h1>
-      
+
     </div>
   );
 };
