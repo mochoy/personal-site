d06cbca4412d0f10082f154f6b7355377e07613f
Monty <montgomery.okdaily@gmail.com>
2020-12-07 22:32:05 -0800

    Create file for first post data like date and stuff

---
+++ src/assets/blog/first_post/index.js
@@ -0,0 +1,11 @@
+import first_post from './first_post.md';
+
+const firstPost = {
+  isDeployed: true,
+  date: "Monday, December 07, 2020",
+  title: "First Post!",
+  url: "first_post",
+  post: first_post
+};
+
+export default firstPost;
