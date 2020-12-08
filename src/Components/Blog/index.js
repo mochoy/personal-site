78ff316dc3c19d1ec068b897a499ebfdb770a24d
Monty <montgomery.okdaily@gmail.com>
2020-12-08 14:01:58 -0800

    Attempt to fetch and parse posts in blog index file

--- src/Components/Blog/index.js
+++ src/Components/Blog/index.js
@@ -1,6 +1,9 @@
 import React, { 
   // useContext 
+  useEffect
 } from 'react';
+
+import ReactMarkdown from 'react-markdown'
 
 // import { ReactGACtx } from '../App';
 
@@ -14,10 +17,23 @@ const Blog = props => {
 
   console.log(posts)
 
+  useEffect(() => {
+    console.log(posts)
+  }, [posts]);
+
   return (
     <div id="Blog">      
       <h1 className="text-center">Posts</h1>
 
+      { posts.map((post, i) => {
+        return (
+          <div key={i}>
+            {post.date}
+          </div>
+        )
+      })
+
+      }
     </div>
   );
 };
--- src/assets/blog/index.js
+++ src/assets/blog/index.js
@@ -1,7 +1,26 @@
 import first_post from './first_post';
 
-const posts = [
+// Array of all posts before fetching and parsing the markdown file
+const postsBeforeFetchingFile = [
   first_post
 ];
+
+// Apply any sort of data init for blog posts, like retreiving and parsing 
+// the markdown file
+const posts = postsBeforeFetchingFile.map(
+  async (postBeforeFetchingFile) => {
+    const { postFile } = postBeforeFetchingFile;
+
+    // Fetch file, then get the text of that file
+    let post = await fetch(postFile).then(res => res.text());
+
+    console.log(post)
+
+    return {
+      ...postsBeforeFetchingFile,
+      post: post
+    } 
+  }
+);
 
 export default posts;
