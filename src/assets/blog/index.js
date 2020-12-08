7781cc825a5617ce581ab6e7928d7138fe14ea20
Monty <montgomery.okdaily@gmail.com>
2020-12-08 14:04:07 -0800

    blog index file now only exports the posts data, no logic to parse or fetch
    will make a sep hook to handle post operations

--- src/assets/blog/index.js
+++ src/assets/blog/index.js
@@ -1,26 +1,9 @@
 import first_post from './first_post';
 
 // Array of all posts before fetching and parsing the markdown file
-const postsBeforeFetchingFile = [
+const posts = [
   first_post
 ];
 
-// Apply any sort of data init for blog posts, like retreiving and parsing 
-// the markdown file
-const posts = postsBeforeFetchingFile.map(
-  async (postBeforeFetchingFile) => {
-    const { postFile } = postBeforeFetchingFile;
-
-    // Fetch file, then get the text of that file
-    let post = await fetch(postFile).then(res => res.text());
-
-    console.log(post)
-
-    return {
-      ...postsBeforeFetchingFile,
-      post: post
-    } 
-  }
-);
 
 export default posts;
