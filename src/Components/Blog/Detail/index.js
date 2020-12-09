5f8918616e35a1d5cb466adf1a70f7773383b8fe
Monty <montgomery.okdaily@gmail.com>
2020-12-09 15:44:32 -0800

    Import useBlogPosts into detail and pass in url

--- src/Components/Blog/Detail/index.js
+++ src/Components/Blog/Detail/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
+import useBlogPosts from '../../../hooks/useBlogPosts';
+
 import './index.css';
 
 const BlogDetail = props => {
+  // Get post based on url: /blog/{post to get}
+  const post = useBlogPosts({ 
+    searchUrl: props.match.params.id
+  });
+
+  console.log(post);
 
   return (
     <div id="BlogDetail">
