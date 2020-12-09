457ce1a9d036de019153f11f18592dd02a9501ad
Monty <montgomery.okdaily@gmail.com>
2020-12-08 18:31:12 -0800

    Add const for maxNumOfCharInBlogPostPrev

--- src/consts.js
+++ src/consts.js
@@ -1,4 +1,8 @@
 // Offset for scrollToElement to account for nav height
 const scrollToOffset = -50;
 
-export { scrollToOffset }
+// Max number of characters to show in a blog post preview. If there 
+// are more characters, then they get truncated and replaced with '...'
+const maxNumOfCharInBlogPostPrev = 50;
+
+export { scrollToOffset, maxNumOfCharInBlogPostPrev };
