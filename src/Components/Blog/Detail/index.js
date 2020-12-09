88d95fae3a375eafc4fe333db5c455da773dec89
Monty <montgomery.okdaily@gmail.com>
2020-12-09 15:21:29 -0800

    Add empty component for BlogDetail

---
+++ src/Components/Blog/Detail/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './index.css';
+
+const BlogDetail = props => {
+
+  return (
+    <div id="BlogDetail">
+      BlogDetail
+    </div>
+  )
+};
+
+export default BlogDetail;
