6f914993aa67320d3f35951fbb0bc7870d809337
Monty <montgomery.okdaily@gmail.com>
2020-12-08 22:14:19 -0800

    Move all blog cover img stuff to own files

--- src/Components/Blog/Cover/index.js
+++ src/Components/Blog/Cover/index.js
@@ -1,1 +1,23 @@
 import React from 'react';
+
+import useWindowSize from '../../../hooks/useWindowSize';
+
+import './index.css';
+
+const Cover = props => {
+  const { height } = useWindowSize();
+
+  return (
+    <div className="cover" style={{ height: height*0.4 + "px" }}>
+      <div className="bg-img"
+        style={{
+          backgroundImage: `url(${require('assets/images/background/blog.jpg')})`
+        }}
+      >
+        <h1 className="text-center cover-title">Posts</h1>
+      </div>
+    </div>
+  )
+};
+
+export default Cover;
--- src/Components/Blog/index.js
+++ src/Components/Blog/index.js
@@ -4,9 +4,10 @@ import React, {
 import { Link } from 'react-router-dom';
 import ReactMarkdown from 'react-markdown'
 
+import Cover from './Cover';
+
 // import { ReactGACtx } from '../App';
 
-import useWindowSize from '../../hooks/useWindowSize';
 import useBlogPosts from '../../hooks/useBlogPosts';
 
 import './index.css';
@@ -16,21 +17,12 @@ import './blog-content.css';
 const Blog = props => {  
   // const ReactGA = useContext(ReactGACtx);
 
-  const { height } = useWindowSize();
   const posts = useBlogPosts();
 
   return (
     <div id="Blog">
-      <div className="cover" style={{ height: height*0.4 + "px" }}>
-        <div className="bg-img"
-          style={{
-            backgroundImage: `url(${require('assets/images/background/blog.jpg')})`
-          }}
-        >
-          <h1 className="text-center cover-title">Posts</h1>
-        </div>
-      </div>
       
+      <Cover/>
 
 
       { posts.map((post, i) => {
