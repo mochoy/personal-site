7e1f8fcdb61e4009cddc6f12e82c933f4e671586
Monty <montgomery.okdaily@gmail.com>
2020-12-09 20:28:27 -0800

    Add dummy post for 2021 internship search

--- src/assets/blog/index.js
+++ src/assets/blog/index.js
@@ -2,13 +2,15 @@ import first_post from './first_post';
 import epic_buck_converter_intro from './epic_buck_converter_intro';
 import how_i_made_this_blog from './how_i_made_this_blog';
 import track_jobs_4_me_review from './track_jobs_4_me_review';
+import summer_2021_internship_search from './summer_2021_internship_search';
 
 // Array of all posts before fetching and parsing the markdown file
 const posts = [
   first_post,
   epic_buck_converter_intro,
   how_i_made_this_blog,
-  track_jobs_4_me_review
+  track_jobs_4_me_review,
+  summer_2021_internship_search
 ];
 
 
---
+++ src/assets/blog/summer_2021_internship_search/index.js
@@ -0,0 +1,12 @@
+import summer_2021_internship_search from './summer_2021_internship_search.md';
+
+const summer2021InternshipSearch = {
+  isDeployed: true,
+  isPreview: true,
+  date: "???",
+  title: "Electrical Engineering Interships: My Summer 2021 Internship Search",
+  url: "summer_2021_internship_search",
+  postFile: summer_2021_internship_search
+};
+
+export default summer2021InternshipSearch;
