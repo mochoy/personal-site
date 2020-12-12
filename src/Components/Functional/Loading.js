73d9bc50f6dc0466e8340617e6742a35f91a699c
Monty <montgomery.okdaily@gmail.com>
2020-12-11 17:47:20 -0800

    Add comments to describe Loading component

--- src/Components/Functional/Loading.js
+++ src/Components/Functional/Loading.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import { BeatLoader } from "react-spinners";
 
+/**
+ * Component that renders loader and loading txt. Can be rendered conditionally
+ * in it's parent component or can pass in isLoading param
+ * 
+ * @param {Boolean} isLoading: true to render loading stuff, else false 
+ * 
+ * @return {JSX} component with loader and loading txt
+ */
 const Loading = props => {
   const { isLoading } = props;
 
