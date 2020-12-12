68358beac149e5346b00ed07765f848918e6496c
Monty <montgomery.okdaily@gmail.com>
2020-12-12 13:18:46 -0800

    Add comments for DateInfo

--- src/Components/Blog/DateInfo/index.js
+++ src/Components/Blog/DateInfo/index.js
@@ -2,6 +2,16 @@ import React from 'react';
 
 import './index.css';
 
+/**
+ * Component that renders date, word count, and reading time of post inline with 
+ * proper styling. When this component was made, it was used in Blog for each 
+ * post and BlogDetail for an entire post, so I decided to decompose it into its
+ * own component so it could be used everywhere easier.
+ * 
+ * @param {String} post: post data, should have date, wordCount, readingTime, props
+ * 
+ * @return {JSX} component with date info stuff 
+ */
 const DateInfo = ({ post }) => {
   const { date, wordCount, readingTime } = post;
 
