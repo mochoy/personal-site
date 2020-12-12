4f92e70cadc64cb325a567d570927b0885678f77
Monty <montgomery.okdaily@gmail.com>
2020-12-11 17:43:03 -0800

    Loading component supports isLoading input

--- src/Components/Functional/Loading.js
+++ src/Components/Functional/Loading.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { BeatLoader } from "react-spinners";
 
 const Loading = props => {
+  const { isLoading } = props;
+
   return (
     <div id="Loading">
       <div className="flex-container-horizontally-center">
-        <BeatLoader loading={true}/>
+        <BeatLoader loading={!!isLoading ? isLoading : true}/>
       </div>
       <p className="text-center">Loading...</p>
     </div>
