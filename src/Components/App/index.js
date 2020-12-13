23872ef035f32b901f978404c567d7c5157023df
Monty <montgomery.okdaily@gmail.com>
2020-12-12 17:24:36 -0800

    Connect to firebase db and print db snapshot

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,5 +1,8 @@
-import React, { createContext } from 'react';
+import React, { createContext, useEffect } from 'react';
 import ReactGA from 'react-ga';
+import firebase from 'firebase/app';
+import 'firebase/database';
+
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 import Nav from '../Nav';
@@ -12,8 +15,9 @@ import Footer from '../Footer';
 
 import './index.css';
 
-// test pat
 
+//
+// GA Stuff
 const initGA = () => {
   ReactGA.initialize(process.env.REACT_APP_GA_KEY, {
     debug: false,       // Set to fale to disable logs
@@ -45,9 +49,32 @@ ReactGA.custom = {
 export const ReactGACtx = createContext(ReactGA);
 
 
+//
+// Firebase stuff
+// Get config info here: https://firebase.google.com/docs/web/setup#add_firebase_to_your_app
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_API,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+
+firebase.initializeApp(firebaseConfig);
+
+
 const App = () => {
-  React.useEffect(() => {
+  const db = firebase.database().ref();
+
+  useEffect(() => {
     initGA();
+
+    db.on('value', snapshot => {
+      console.log(snapshot.val())
+    });
   }, []);
 
   return (
