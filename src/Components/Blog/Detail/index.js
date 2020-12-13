eb2f3df6170ada051e6ff07d014e9aa9e6a684e3
Monty <montgomery.okdaily@gmail.com>
2020-12-12 20:52:28 -0800

    If no votes/comments for this post in db, create new db node for that post

--- src/Components/Blog/Detail/index.js
+++ src/Components/Blog/Detail/index.js
@@ -11,16 +11,40 @@ import { dbRef } from '../../../services/firebase';
 import './index.css';
 
 
+// What to set in db for a new post that wasn't already in db. Includes arr/obj
+// for comments and votes
+const defaultDbPostEntry = { 
+  comments: { 0: {
+    time: Date.now(),
+    username: "init",
+    text: "init"
+  }}, 
+  votes: { 0: { 
+    time: Date.now(),
+    vote: "init"
+  }} 
+};
+
+
 const BlogDetail = props => {
   // Get post based on url: /blog/{post to get}
   const [ post, isLoading ] = useBlogPosts({ 
     searchUrl: props.match.params.id
   });
 
+  // Connect to db and get vote/comment info for this post
   const db = dbRef(`blog/${props.match.params.id}`);
 
   db.on('value', snapshot => {
-    console.log(snapshot.val())
+    const contents = snapshot.val();
+
+    // If no contents, means no votes/comments data for this post yet, create a
+    // new obj in db to store votes and posts
+    if (contents === null) {
+      // db.push will push to ref of db, which will be blog.{post_url}
+      // This is the standard format for comments/votes
+      db.set(defaultDbPostEntry);
+    }
   });
 
   
