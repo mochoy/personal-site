f087dbc21ee2326831058790a15f839f86b4145d
Monty <montgomery.okdaily@gmail.com>
2020-12-12 17:47:27 -0800

    Move firebase init stuff to firebase service file

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,7 +1,4 @@
 import React, { createContext, useEffect } from 'react';
-import firebase from 'firebase/app';
-import 'firebase/database';
-
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 import Nav from '../Nav';
@@ -16,31 +13,10 @@ import ReactGA from '../../services/reactGA';
 import './index.css';
 
 
-
-
-
 export const ReactGACtx = createContext(ReactGA);
 
 
-//
-// Firebase stuff
-// Get config info here: https://firebase.google.com/docs/web/setup#add_firebase_to_your_app
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_API,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
-};
-
-firebase.initializeApp(firebaseConfig);
-
-
 const App = () => {
-  const db = firebase.database().ref();
 
   useEffect(() => {
     ReactGA.initGA();
