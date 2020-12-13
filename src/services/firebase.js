2f38c3800e777a563dfeadd94219c4de3e8566bf
Monty <montgomery.okdaily@gmail.com>
2020-12-12 18:13:54 -0800

    move firebase config outside of init()

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -4,23 +4,18 @@ import firebase from 'firebase/app';
 import 'firebase/database';
 
 
-// Init firebase app and connect to db. Returns ref to db
-const init = () => {
-  // Get config info here: https://firebase.google.com/docs/web/setup#add_firebase_to_your_app
-  const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_API,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
-  };
+// Get config info here: https://firebase.google.com/docs/web/setup#add_firebase_to_your_app
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_API,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
 
-  firebase.initializeApp(firebaseConfig);
-
-  return firebase.database().ref();
 // Init firebase app and connect to db. Returns ref to db
 const init = (ref) => {
   if (firebase.apps.length === 0) {
