d3556aa1e03ca79e8bc82c39977f71a52629aebe
Monty <montgomery.okdaily@gmail.com>
2020-12-13 20:52:59 -0800

    Add tooltop on likes

--- src/Components/Blog/Detail/UserInteraction/Likes.js
+++ src/Components/Blog/Detail/UserInteraction/Likes.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import { ThumbUpAltOutlined, ThumbUp } from '@material-ui/icons';
+import ReactTooltip from 'react-tooltip';
 
 
 const Likes = props => {
   const { likes, likeStatus, onLikeEvent } = props;
 
   return (
-    <div id="Likes">
+    <div id="Likes" className="flex-container-vertically-center">
       { likeStatus === "unliked" &&
-        <ThumbUpAltOutlined fontSize="large" className="mouse-on-hover"
+        <ThumbUpAltOutlined fontSize="default" className="mouse-on-hover"
+          data-tip data-for='like-tooltip' 
           onClick={() => onLikeEvent("liked")}
         />
       }
       { likeStatus === "liked" &&
-        <ThumbUp fontSize="large" className="mouse-on-hover"
+        <ThumbUp fontSize="default" className="mouse-on-hover"
+          data-tip data-for='like-tooltip' 
           onClick={() => onLikeEvent("unliked")}
         />
       }
-      
 
+      <ReactTooltip id='like-tooltip' effect='solid'>
+        <p style={{ margin: 0 }}>{likeStatus === "unliked" ? "Like" : "Unlike"}</p>
+      </ReactTooltip>
     </div>
   )
 };
