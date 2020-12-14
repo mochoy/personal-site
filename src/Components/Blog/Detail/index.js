db2e76d7565f2bce112b1c1d6d690fd05deb5a1c
Monty <montgomery.okdaily@gmail.com>
2020-12-13 17:25:01 -0800

    Attempt to memoize blog detail render based on form vals
    perf slow, but this didn't fix it, I suspect attempts to re-render md is
    slowing things down a lot

--- src/Components/Blog/Detail/index.js
+++ src/Components/Blog/Detail/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { memo } from 'react';
 import { Redirect } from 'react-router-dom';
 import ReactMarkdown from 'react-markdown';
 
@@ -12,62 +12,83 @@ import useCommentsAndVotesForBlog from '../../../hooks/useCommentsAndVotesForBlo
 import './index.css';
 
 
-const BlogDetail = props => {
-  // Get post based on url: /blog/{post to get}
-  const [ post, isBlogPostLoading ] = useBlogPosts({ 
-    searchUrl: props.match.params.id
-  });
+const BlogDetail = memo(
+  props => {
+    // Get post based on url: /blog/{post to get}
+    const [ post, isBlogPostLoading ] = useBlogPosts({ 
+      searchUrl: props.match.params.id
+    });
 
-  const { comments, 
-    votes, 
+    const { comments, 
+      votes, 
+      
+      name,
+      updateName,
+      comment,
+      updateComment } = useCommentsAndVotesForBlog(
+      props.location.pathname
+    );
+
+
+    // console.log("Comments: ", comments)
+    // console.log("votes: ", votes)
+
+    console.log("rendering")
+
     
-    name,
-    updateName,
-    comment,
-    updateComment } = useCommentsAndVotesForBlog(
-    props.location.pathname
-  );
+    // Blog post is loading
+    if (isBlogPostLoading) {
+      return <div id="BlogDetail"><Loading/></div>
 
+    // No post found, so invalid link, reroute to 404
+    } else if (post.length === 0) {
+      return <Redirect to='/404' />
+    
+    // Render actual post
+    } else {  
+      const { title, md, filePath } = post[0];
+    
+      // Md renderers
+      const renderers = {
+        // Render for img, make sure get correct path to image via require()
+        image: ({src, alt}) => {
+          return <img src={require(`assets/blog/${filePath}/${src}`)} alt={alt} />
+        }
+      };
 
-  // console.log("Comments: ", comments)
-  // console.log("votes: ", votes)
+      return (
+        <div id="BlogDetail" className="blog-content">
+          <h1 id="title">{title}</h1>
+          <DateInfo post={post[0]} />
+          <ReactMarkdown className="md" renderers={renderers} source={md}/>
 
-  
-  // Blog post is loading
-  if (isBlogPostLoading) {
-    return <div id="BlogDetail"><Loading/></div>
+          <Comments comments={comments} 
+            name={name}
+            updateName={updateName}
+            comment={comment}
+            updateComment={updateComment}
+          />
+        </div>
+      )
+    }
+  },
+  // Determine whether or not to re-render based on props only
+  // To re-render, return false
+  (prevProps, nextProps) => {
+    const prevName = prevProps.name;
+    const prevComment = prevProps.comment;
 
-  // No post found, so invalid link, reroute to 404
-  } else if (post.length === 0) {
-    return <Redirect to='/404' />
-  
-  // Render actual post
-  } else {  
-    const { title, md, filePath } = post[0];
-  
-    // Md renderers
-    const renderers = {
-      // Render for img, make sure get correct path to image via require()
-      image: ({src, alt}) => {
-        return <img src={require(`assets/blog/${filePath}/${src}`)} alt={alt} />
-      }
-    };
+    const nextName = nextProps.name;
+    const nextComment = nextProps.comment;
 
-    return (
-      <div id="BlogDetail" className="blog-content">
-        <h1 id="title">{title}</h1>
-        <DateInfo post={post[0]} />
-        <ReactMarkdown className="md" renderers={renderers} source={md}/>
+    // If comments or name is the same, don't re-render/return true, else false
+    if (prevName === nextName || prevComment === nextComment) {
+      return true
+    }
 
-        <Comments comments={comments} 
-          name={name}
-          updateName={updateName}
-          comment={comment}
-          updateComment={updateComment}
-        />
-      </div>
-    )
+    return false
   }
-};
+
+);
 
 export default BlogDetail;
