16b068b527fd0df83e51fa22c17a2b590b766bb9
Monty <montgomery.okdaily@gmail.com>
2020-12-15 18:18:18 -0800

    Create algo to generate table of contents tree

--- src/hooks/useBlogPosts.js
+++ src/hooks/useBlogPosts.js
@@ -50,6 +50,8 @@ const useBlogPosts = params => {
             const previewMd = generatePreviewMd(postText);
 
             const wordCount = postText.trim().split(/\s+/).length;
+
+            const tableOfContents = generateTableOfContents(postText);
             
             // Final object to represent the post and its corresponding metadata
             return {
@@ -61,7 +63,9 @@ const useBlogPosts = params => {
               url: "/blog/" + post.url, // full url: .com/{url}
 
               md: postText,
-              previewMd: previewMd
+              previewMd: previewMd,
+              
+              tableOfContents: tableOfContents
             }
           })  // Map
       );  // Promise
@@ -94,6 +98,114 @@ const generatePreviewMd = postText => {
   }
 
   return previewMd;
+}
+
+// Generate table of contents based on header elements. 
+// Returns ToC as an object in this form: 
+// [
+//   {
+//     value: "",
+//     children: [
+//       { value: "" },
+//       {
+//         value: "",
+//         children: [
+//           { value: "" }
+//         ]
+//       }
+//     ]
+//   }, 
+//   {
+//     value: ""
+//   }
+// ]
+//
+// Algo: 
+//  - Vars to track of most recent of each hierarchy
+//  Data fixing:
+//    - Split lines, but only deal with header lines
+//    - For each header line, get hierarchy of header (ex: # = 1, ### = 3) and
+//      header text
+//  Tree population:
+//    - Put current header as a child under the most recent previous header that
+//      is 1 greater in hierarchy (ex, ## would go right under most recent #)
+//    - Set this node as the most recent node in its hierarchy
+//    - Clear other pointers that have less hierarchy (current node ##, clear 
+//      ### and ####). This is to ensure that the next (following prev ex) ###
+//      gets into the current ##, not the previous ##
+const generateTableOfContents = postText => {
+  let ToC = [];
+
+  // Pointers to the most recent of each hierarchy
+  let lastH1, lastH2, lastH3, lastH4;
+
+  postText.split('\n')
+    // If first char of the line is a #, then it's a heading
+    .filter(line => line[0] === "#")
+    .map(line => {
+      // The #'s at the beginning of the line are sep by a space before the txt,
+      // so can use that pattern to get how many # there are to determine the 
+      // heading hierarchy
+      const splitLine = line.split(" ");
+
+
+      // Hierarchy of the heading, 1 for lowest hierarchy/biggest txt
+      const headingHierarchy = splitLine[0].length;
+
+      // Text of actual heading
+      const headingTxt = splitLine.slice(1, splitLine.length).join(" ");
+
+      // Node for this heading
+      const node = {
+        value: headingTxt,
+        children: []
+      }
+
+      // console.log(`${splitLine[0]}, ${headingHierarchy}: ${headingTxt}`);
+      
+      // Push nodes and set pointers
+      switch (headingHierarchy) {
+        // Push h1's direclty into ToC at root level, clear other node pointers
+        case 1:
+          ToC.push(node);
+          lastH1 = node;
+
+          lastH2 = null;
+          lastH3 = null;
+          lastH4 = null;
+          
+          break;
+        // Push h2's directly to mose recent h1, set this h2 as most recent h2, 
+        // clear other node pointers
+        case 2:
+          lastH1.children.push(node);
+          lastH2 = node;
+
+          lastH3 = null;
+          lastH4 = null;
+
+          break;
+        // Push h3's directly to mose recent h2, set this h3 as most recent h3, 
+        // clear other node pointers
+        case 3:
+          lastH2.children.push(node);
+          lastH3 = node;
+
+          lastH4 = null;
+
+          break;
+
+        // Push h4's directly to mose recent h3, set this h4 as most recent h4
+        case 3:
+          lastH3.children.push(node);
+          lastH4 = node;
+
+          break;
+      }
+
+    });
+
+  return ToC;
 }
 
 export default useBlogPosts;
