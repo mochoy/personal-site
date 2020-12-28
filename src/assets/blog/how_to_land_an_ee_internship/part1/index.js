0bf8cfb1ecfb795b7a9834131d5f7fabcf0042f2
Monty <montgomery.okdaily@gmail.com>
2020-12-28 15:40:05 -0800

    Refactor ee internship post to support multiple parts
    all parts will be nested in the same folder

---
+++ src/assets/blog/how_to_land_an_ee_internship/part1/index.js
@@ -0,0 +1,12 @@
+import part1 from './part1.md';
+
+const howToLandAnEEInternshipPart1 = {
+  isDeployed: true,
+  isPreview: false,
+  date: "???",
+  title: "How to Land an Electrical Engineering Internship Part 1: The Internship Mindset",
+  filePath: "how_to_land_an_ee_internship/part1",
+  postFile: part1
+};
+
+export default howToLandAnEEInternshipPart1;
--- src/assets/blog/index.js
+++ src/assets/blog/index.js
@@ -4,11 +4,12 @@ import how_i_made_this_blog from './how_i_made_this_blog';
 import track_jobs_4_me_review from './track_jobs_4_me_review';
 import summer_2021_internship_search from './summer_2021_internship_search';
 import remote_microsoft_internship from './remote_microsoft_internship';
-import how_to_land_an_internship from './how_to_land_an_internship';
+
+import howToLandAnEEInternshipPart1 from './how_to_land_an_ee_internship/part1';
 
 // Array of all posts before fetching and parsing the markdown file
 const posts = [
-  how_to_land_an_internship,
+  howToLandAnEEInternshipPart1,
   first_post,
   remote_microsoft_internship,
   summer_2021_internship_search,
