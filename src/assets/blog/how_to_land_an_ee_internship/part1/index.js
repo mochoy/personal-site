dcd0c15b244f6e0dd74177c8fd1a8d3209ae089b
Monty <montgomery.okdaily@gmail.com>
2020-12-28 15:43:50 -0800

    Add date to ee internship part 1

--- src/assets/blog/how_to_land_an_ee_internship/part1/index.js
+++ src/assets/blog/how_to_land_an_ee_internship/part1/index.js
@@ -3,7 +3,7 @@ import part1 from './part1.md';
 const howToLandAnEEInternshipPart1 = {
   isDeployed: true,
   isPreview: false,
-  date: "???",
+  date: "Monday, December 28, 2020",
   title: "How to Land an Electrical Engineering Internship Part 1: The Internship Mindset",
   filePath: "how_to_land_an_ee_internship/part1",
   postFile: part1
