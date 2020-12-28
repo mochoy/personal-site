31a1e5504d9ccff193b359123051c4093d80a06d
Monty <montgomery.okdaily@gmail.com>
2020-12-28 11:54:21 -0800

    Remove unused functions that render old ToC

--- src/Components/Blog/Detail/TableOfContents/index.js
+++ src/Components/Blog/Detail/TableOfContents/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { Drawer } from '@material-ui/core';
 import Scrollspy from 'react-scrollspy'
 
@@ -16,43 +15,6 @@ const TableOfContents = props => {
 
   // Array of all toc items in order of appearance
   let tocItems = [];
-
-  // Recurisively renders all links in a ToC object
-  const renderToCLinks = (node, i) => {
-    const { value, headingHierarchy, url, children } = node;
-
-    return (
-      <div key={i}>
-        <Link to={url}
-          onClick={() => scrollToElement(url, { offset: scrollToOffset })}
-        >
-          { renderToCText(value, headingHierarchy, url) }
-        </Link>
-
-        {children.map(renderToCLinks)}
-      </div>
-      
-    )
-  };
-
-  // Renders appropriate h-tag for ToC based on heading hierarchy
-  const renderToCText = (value, headingHierarchy, url) => {
-    tocItems.push(url.replace("#", ""));
-
-    switch (headingHierarchy) {
-      case 1: 
-        return <h1><p>{value}</p></h1>
-      case 2: 
-        return <h2><p>{value}</p></h2>
-      case 3: 
-        return <h3><p>{value}</p></h3>
-      case 4: 
-        return <h4><p>{value}</p></h4>
-      default: 
-        return;
-    }
-  };
-
 
   // Flatten table of contents into a single array of objs so no recursion is 
   // needed to render anything. Scrollspy seems to have trouble dealing with
