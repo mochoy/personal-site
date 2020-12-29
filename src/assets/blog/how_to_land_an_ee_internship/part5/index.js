8779364dca2fbff7e6aeda3f02e831be160d674e
Monty <montgomery.okdaily@gmail.com>
2020-12-28 16:04:42 -0800

    Render and add files for part 5

---
+++ src/assets/blog/how_to_land_an_ee_internship/part5/index.js
@@ -0,0 +1,12 @@
+import part5 from './part5.md';
+
+const howToLandAnEEInternshipPart5 = {
+  isDeployed: true,
+  isPreview: false,
+  date: "???",
+  title: "How to Land an Electrical Engineering Internship Part 5: Interviews",
+  filePath: "how_to_land_an_ee_internship/part5",
+  postFile: part5
+};
+
+export default howToLandAnEEInternshipPart5;
--- src/assets/blog/index.js
+++ src/assets/blog/index.js
@@ -9,6 +9,7 @@ import howToLandAnEEInternshipPart1 from './how_to_land_an_ee_internship/part1';
 import howToLandAnEEInternshipPart2 from './how_to_land_an_ee_internship/part2';
 import howToLandAnEEInternshipPart3 from './how_to_land_an_ee_internship/part3';
 import howToLandAnEEInternshipPart4 from './how_to_land_an_ee_internship/part4';
+import howToLandAnEEInternshipPart5 from './how_to_land_an_ee_internship/part5';
 
 // Array of all posts before fetching and parsing the markdown file
 const posts = [
@@ -16,6 +17,7 @@ const posts = [
   howToLandAnEEInternshipPart2,
   howToLandAnEEInternshipPart3,
   howToLandAnEEInternshipPart4,
+  howToLandAnEEInternshipPart5,
   first_post,
   remote_microsoft_internship,
   summer_2021_internship_search,
