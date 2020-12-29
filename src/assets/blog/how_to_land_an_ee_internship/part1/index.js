01f89b5141d2b1e31637815373603101419292a3
Monty <montgomery.okdaily@gmail.com>
2020-12-28 16:32:13 -0800

    Add next prop to part 1 that's link to part 2

--- src/assets/blog/how_to_land_an_ee_internship/part1/index.js
+++ src/assets/blog/how_to_land_an_ee_internship/part1/index.js
@@ -6,7 +6,9 @@ const howToLandAnEEInternshipPart1 = {
   date: "Monday, December 28, 2020",
   title: "How to Land an Electrical Engineering Internship Part 1: The Internship Mindset",
   filePath: "how_to_land_an_ee_internship/part1",
-  postFile: part1
+  postFile: part1,
+
+  next: "how_to_land_an_electrical_engineering_internship_part_2_pre-application"
 };
 
 export default howToLandAnEEInternshipPart1;
