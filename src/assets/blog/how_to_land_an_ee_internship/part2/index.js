355f0715d8fecef1d3002e5cef464bcd64856611
Monty <montgomery.okdaily@gmail.com>
2020-12-29 11:12:25 -0800

    Add prev and next posts to part 2

--- src/assets/blog/how_to_land_an_ee_internship/part2/index.js
+++ src/assets/blog/how_to_land_an_ee_internship/part2/index.js
@@ -6,7 +6,10 @@ const howToLandAnEEInternshipPart2 = {
   date: "???",
   title: "How to Land an Electrical Engineering Internship Part 2: Pre-Application",
   filePath: "how_to_land_an_ee_internship/part2",
-  postFile: part2
+  postFile: part2,
+
+  prev: "how_to_land_an_electrical_engineering_internship_part_1_the_internship_mindset",
+  next: "how_to_land_an_electrical_engineering_internship_part_3_your_application"
 };
 
 export default howToLandAnEEInternshipPart2;
