00e35b2087351844cf360e318ffc8b75dbc12d99
Monty <montgomery.okdaily@gmail.com>
2020-12-29 16:48:04 -0800

    Add r ga events for like/unlike

--- src/Components/Blog/Detail/UserInteraction/Likes.js
+++ src/Components/Blog/Detail/UserInteraction/Likes.js
@@ -1,23 +1,41 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { ThumbUpAltOutlined, ThumbUp } from '@material-ui/icons';
 import ReactTooltip from 'react-tooltip';
+
+import { ReactGACtx } from '../../../App';
 
 
 const Likes = props => {
   const { likes, likeStatus, onLikeEvent } = props;
+
+  const ReactGA = useContext(ReactGACtx);
 
   return (
     <div id="Likes" className="flex-container-vertically-center">
       { likeStatus === "unliked" &&
         <ThumbUpAltOutlined className="mouse-on-hover"
           data-tip data-for='like-tooltip' 
-          onClick={() => onLikeEvent("liked")}
+          onClick={() => {
+            onLikeEvent("liked");
+
+            ReactGA.event({
+              category: 'Blog Post',
+              action: 'Liked'
+            });
+          }}
         />
       }
       { likeStatus === "liked" &&
         <ThumbUp className="mouse-on-hover"
           data-tip data-for='like-tooltip' 
-          onClick={() => onLikeEvent("unliked")}
+          onClick={() => {
+            onLikeEvent("unliked");
+
+            ReactGA.event({
+              category: 'Blog Post',
+              action: 'Unliked'
+            });
+          }}
         />
       }
 
