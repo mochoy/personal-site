fcbc232b3393ae8e4e0363c5c545607bf66c68da
Monty <montgomery.okdaily@gmail.com>
2021-06-11 19:53:05 -0700

    move mstf blog post out of preview

--- src/assets/blog/remote_microsoft_internship/index.js
+++ src/assets/blog/remote_microsoft_internship/index.js
@@ -2,7 +2,7 @@ import remote_microsoft_internship from './remote_microsoft_internship.md';
 
 const remoteMicrosoftInternship = {
   isDeployed: true,
-  isPreview: true,
+  isPreview: false,
   date: "???",
   title: "My Remote Software Engineering Internship Experience at Microsoft (Summer 2020)",
   url: "my_remote_software_engineering_internship_experience_at_microsoft_summer_2020",
