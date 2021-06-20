b6bf2f29dc83ff9dd29fd633ce8ed3494d267f98
Monty <montgomery.okdaily@gmail.com>
2021-06-20 14:44:43 -0700

    put post 8 back to preview mode

--- src/assets/blog/how_to_land_an_ee_internship/part8-closing/index.js
+++ src/assets/blog/how_to_land_an_ee_internship/part8-closing/index.js
@@ -2,7 +2,7 @@ import part8 from './part8.md';
 
 const howToLandAnEEInternshipPart8 = {
   isDeployed: true,
-  isPreview: false,
+  isPreview: true,
   date: "???",
   title: "How to Land an Electrical Engineering Internship Part 8: Closing",
   filePath: "how_to_land_an_ee_internship/part8-closing",
