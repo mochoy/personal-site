217c12b2baccc15927f46706857b5191fa4fe300
Monty <montgomery.okdaily@gmail.com>
2021-06-20 14:49:33 -0700

    rename part 5 to part6-interviews

---
+++ src/assets/blog/how_to_land_an_ee_internship/part6-interviews/index.js
@@ -0,0 +1,12 @@
+import part6 from './part6.md';
+
+const howToLandAnEEInternshipPart6 = {
+  isDeployed: true,
+  isPreview: true,
+  date: "???",
+  title: "How to Land an Electrical Engineering Internship Part 6: Interviews",
+  filePath: "how_to_land_an_ee_internship/part6-interviews",
+  postFile: part6
+};
+
+export default howToLandAnEEInternshipPart6;
--- src/assets/blog/index.js
+++ src/assets/blog/index.js
@@ -10,6 +10,7 @@ import howToLandAnEEInternshipPart1 from './how_to_land_an_ee_internship/part1-i
 // import howToLandAnEEInternshipPart3 from './how_to_land_an_ee_internship/part3-your_application';
 // import howToLandAnEEInternshipPart4 from './how_to_land_an_ee_internship/part4-applying';
 // import howToLandAnEEInternshipPart5 from './how_to_land_an_ee_internship/part5';
+// import howToLandAnEEInternshipPart6 from './how_to_land_an_ee_internship/part6-interviews';
 // import howToLandAnEEInternshipPart7 from './how_to_land_an_ee_internship/part7-offer';
 // import howToLandAnEEInternshipPart8 from './how_to_land_an_ee_internship/part8-closing';
 
@@ -17,7 +18,7 @@ import howToLandAnEEInternshipPart1 from './how_to_land_an_ee_internship/part1-i
 const posts = [
   remote_microsoft_internship,
   howToLandAnEEInternshipPart1,
-  // howToLandAnEEInternshipPart7,
+  // howToLandAnEEInternshipPart6,
   first_post
   
   // howToLandAnEEInternshipPart3,
