d06b6a925a8d7b15f0f631159a8a760cc98ea27d
Monty <montgomery.okdaily@gmail.com>
2021-06-20 14:07:15 -0700

    Change footer date to 2021

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -18,7 +18,7 @@ const Footer = props => {
         <p>Dope home image credit IG: robertklim</p>
         <p>Blog cover image credit: Andrew Neel</p>
         <p className="text-center">Monty Choy</p>
-        <p className="text-center">2017 - 2020</p>
+        <p className="text-center">2017 - 2021</p>
       </div>
     </VisibilitySensor>
   );
