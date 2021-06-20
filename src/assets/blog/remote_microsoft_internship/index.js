e262f3c99df462ea6a3afe04e5d4a34ec3ac90a0
Monty <montgomery.okdaily@gmail.com>
2021-06-20 12:03:52 -0700

    change date on mstf post and put it first in posts

--- src/assets/blog/remote_microsoft_internship/index.js
+++ src/assets/blog/remote_microsoft_internship/index.js
@@ -3,7 +3,7 @@ import remote_microsoft_internship from './remote_microsoft_internship.md';
 const remoteMicrosoftInternship = {
   isDeployed: true,
   isPreview: false,
-  date: "???",
+  date: "Thursday, June 3, 2021",
   title: "My Remote Software Engineering Internship Experience at Microsoft (Summer 2020)",
   url: "my_remote_software_engineering_internship_experience_at_microsoft_summer_2020",
   filePath: "remote_microsoft_internship",
