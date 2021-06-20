4ade8cdc99f2440cc2e9dd2f485c8c0c45286e9f
Monty <montgomery.okdaily@gmail.com>
2021-06-20 13:00:50 -0700

    make sure in prod before incrementing blog post view analytics

--- src/hooks/useBlogPostViewAnalytics.js
+++ src/hooks/useBlogPostViewAnalytics.js
@@ -3,6 +3,8 @@ import { useLocation } from 'react-router-dom';
 
 import { FirebaseCtx } from '../Components/App';
 
+import { isProd } from '../helpers/isDev';
+
 /**
  * Hook that incrememnts view count for that specific post
  * 
@@ -15,8 +17,8 @@ const useBlogPostViewAnalytics = post => {
 
   // Increment views counter in GA, should increment if post changes
   useEffect(() => {
-    // Make sure there's actually a post and it's not just loading
-    if (!!post && post.length > 0) {
+    // Make sure in prod there's actually a post and it's not just loading
+    if (isProd() && !!post && post.length > 0) {
       // Increment views in db
       db.child('views').transaction(currentViews => currentViews + 1);
     }
