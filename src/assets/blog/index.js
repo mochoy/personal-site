79d26a95277e7b61c7507474a255cc8e1bcf25d5
Monty <montgomery.okdaily@gmail.com>
2021-06-20 14:34:12 -0700

    rename part 4 folder to part4-applying

--- src/assets/blog/index.js
+++ src/assets/blog/index.js
@@ -8,7 +8,7 @@ import remote_microsoft_internship from './remote_microsoft_internship';
 import howToLandAnEEInternshipPart1 from './how_to_land_an_ee_internship/part1-internship_mindset';
 // import howToLandAnEEInternshipPart2 from './how_to_land_an_ee_internship/part2-pre-application';
 // import howToLandAnEEInternshipPart3 from './how_to_land_an_ee_internship/part3-your_application';
-// import howToLandAnEEInternshipPart4 from './how_to_land_an_ee_internship/part4';
+// import howToLandAnEEInternshipPart4 from './how_to_land_an_ee_internship/part4-applying';
 // import howToLandAnEEInternshipPart5 from './how_to_land_an_ee_internship/part5';
 // import howToLandAnEEInternshipPart6 from './how_to_land_an_ee_internship/part6';
 // import howToLandAnEEInternshipPart7 from './how_to_land_an_ee_internship/part7';
@@ -17,7 +17,7 @@ import howToLandAnEEInternshipPart1 from './how_to_land_an_ee_internship/part1-i
 const posts = [
   remote_microsoft_internship,
   howToLandAnEEInternshipPart1,
-  // howToLandAnEEInternshipPart3,
+  // howToLandAnEEInternshipPart4,
   first_post
   
   // howToLandAnEEInternshipPart3,
