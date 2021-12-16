78bf1e6963e1600fc80cd785837d53f6fe8ac1bc
Monty Choy <montgomery.okdaily@gmail.com>
2021-12-15 16:27:45 -0800

    create and display blog post for internship interview questions

---
+++ src/assets/blog/internship_interview_questions/index.js
@@ -0,0 +1,12 @@
+import internship_interview_questions from './internship_interview_questions.md';
+
+const internshipInterviewQuestions = {
+  isDeployed: true,
+  date: "Wednesday, December 15, 2021",
+  title: "The Mega List of Electrical Engineering Internship Questions",
+  url: "internship_interview_questions",
+  filePath: "internship_interview_questions",
+  postFile: internship_interview_questions
+};
+
+export default internshipInterviewQuestions;
---
+++ src/assets/blog/internship_interview_questions/internship_interview_questions.md
@@ -0,0 +1,1 @@
+internship_interview_questions
