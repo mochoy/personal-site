c98b667210d678785f0f308d9808992bceebfa70
Monty Choy <montgomery.okdaily@gmail.com>
2021-12-20 10:40:43 -0800

    rename post from mega to ultimate

--- src/assets/blog/internship_interview_questions/index.js
+++ src/assets/blog/internship_interview_questions/index.js
@@ -3,8 +3,8 @@ import internship_interview_questions from './internship_interview_questions.md'
 const internshipInterviewQuestions = {
   isDeployed: true,
   date: "Wednesday, December 15, 2021",
-  title: "The Mega List of Hardware Engineering Internship Intreview Questions",
-  url: "the_mega_list_of_hardware_engineering_internship_interview_questions",
+  title: "The Ultimate List of Hardware Engineering Internship Intreview Questions",
+  url: "the_ultimate_list_of_hardware_engineering_internship_interview_questions",
   filePath: "internship_interview_questions",
   postFile: internship_interview_questions
 };
