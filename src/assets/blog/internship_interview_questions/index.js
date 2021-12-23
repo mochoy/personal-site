5f2ffca0489acb268e4fee3541de22a6f69ef3d9
Monty Choy <montgomery.okdaily@gmail.com>
2021-12-22 18:57:08 -0800

    change date of post to today

--- src/assets/blog/internship_interview_questions/index.js
+++ src/assets/blog/internship_interview_questions/index.js
@@ -2,7 +2,7 @@ import internship_interview_questions from './internship_interview_questions.md'
 
 const internshipInterviewQuestions = {
   isDeployed: true,
-  date: "Wednesday, December 15, 2021",
+  date: "Wednesday, December 22, 2021",
   title: "The Ultimate List of Hardware Engineering Internship Interview Questions",
   url: "the_ultimate_list_of_hardware_engineering_internship_interview_questions",
   filePath: "internship_interview_questions",
